cdc2ac53280c4624c060014700a63e97be222d98
CLE <christopher.leppel@icarus-consult.de>
2017-11-07 12:13:37 +0100

    Bug DoubleOf test (#47)
    * test: test for DoubleOf
    * test: clean test
    
    closes #46

---
+++ tests/Yaapii.Atoms.Tests/Primitives/DoubleOfTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xunit;
+using Yaapii.Atoms.Text;
+
+namespace Yaapii.Atoms.Tests.Primitives
+{
+    public sealed class DoubleOfTests
+    {
+        [Fact]
+        public void ConvertsDouble()
+        {
+            var piStr = Math.PI.ToString("G17");
+            var piFromStr = new DoubleOf(piStr, CultureInfo.CurrentCulture).Value();
+            var piStr2 = piFromStr.ToString("G17");
+
+            Assert.True(piStr == piStr2);
+        }
+    }
+}
