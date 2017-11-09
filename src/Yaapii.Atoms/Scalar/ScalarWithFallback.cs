ea180a5caa33712dbe86a903128552b6e894cccd
Meerow <meerow@mirmx.de>
2017-11-09 16:21:13 +0100

    feat: refactor and ScalarWithFallback (#55)
    * refactor: better name for type
    * refactor: temporarily restore iproc
    * refactor: rename ICallable to IFunc, IProc to IFunc. Closes #41
    * refactor: rename ICallable and IProc to IFunc and IAction.
    * refactor: move to text namespace
    * refactor: move to primitives folder
    * fix: remove preamble from bytes if present
    closes #32
    * refactor: improve readability
    * refactor: change namespaces to do like IO.LengthOf in test projects
    * fix: implemented GetEnumerator()
    * docs: remove type parameter hint
    * refactor: migrate to new LengthOf class
    * fix: ItemAt from empty list
    * fix: docs
    * feat: add ScalarWithFallback class

---
+++ src/Yaapii.Atoms/Scalar/ScalarWithFallback.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yaapii.Atoms.Scalar
+{
+    /// <summary>
+    /// Scalar which calls a fallback function if Value() fails.
+    /// </summary>
+    /// <typeparam name="Out">Type of output value</typeparam>
+    public class ScalarWithFallback<Out> : IScalar<Out>
+    {
+        private readonly IScalar<Out> _origin;
+        private readonly Func<Exception, Out> _fallback;
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="origin">Scalar which can fail</param>
+        /// <param name="fallback">Fallback if scalar fails</param>
+        public ScalarWithFallback(IScalar<Out> origin, Out fallback) : this(
+            origin, 
+            (ex) => fallback)
+        { }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="origin">Scalar which can fail</param>
+        /// <param name="fallback">Fallback if scalar fails</param>
+        public ScalarWithFallback(IScalar<Out> origin, Func<Out> fallback) : this(
+            origin, 
+            (ex) => fallback.Invoke())
+        { }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="origin">scalar which can fail</param>
+        /// <param name="fbk">fallback to apply when fails</param>
+        public ScalarWithFallback(IScalar<Out> origin, Func<Exception, Out> fbk)
+        {
+            _origin = origin;
+            _fallback = fbk;
+        }
+
+        /// <summary>
+        /// Get the value or fallback if fails
+        /// </summary>
+        /// <returns>value or fallback value</returns>
+        public Out Value()
+        {
+            try
+            {
+                return _origin.Value();
+            }
+            catch (Exception ex)
+            {
+                return _fallback.Invoke(ex);
+            }
+
+        }
+    }
+}
