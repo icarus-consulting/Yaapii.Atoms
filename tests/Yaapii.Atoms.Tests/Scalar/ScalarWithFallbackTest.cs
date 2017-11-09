c86c448f8577755bac0cd34348846bc49bd7a2ce
Miro Tunk <miro.tunk@icarus-consult.de>
2017-11-09 16:59:53 +0100

    feat: new class ScalarWithFallback

---
+++ tests/Yaapii.Atoms.Tests/Scalar/ScalarWithFallbackTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Yaapii.Atoms.Scalar.Tests
+{
+    public class ScalarWithFallbackTest
+    {
+        [Fact]
+        public void GivesFallback()
+        {
+            var fbk = "strong string";
+
+            Assert.True(
+                new ScalarWithFallback<string>(
+                    new ScalarOf<string>(
+                        () => throw new Exception("NO STRINGS ATTACHED HAHAHA")),
+                    fbk
+                    ).Value() == fbk);
+        }
+
+        [Fact]
+        public void GivesFallbackByFunc()
+        {
+            var fbk = "strong string";
+
+            Assert.True(
+                new ScalarWithFallback<string>(
+                    new ScalarOf<string>(
+                        () => throw new Exception("NO STRINGS ATTACHED HAHAHA")),
+                    () => fbk
+                    ).Value() == fbk);
+        }
+
+        [Fact]
+        public void InjectsException()
+        {
+            var notAmused = new Exception("All is broken :(");
+
+            Assert.True(
+                new ScalarWithFallback<string>(
+                    new ScalarOf<string>(
+                        () => throw notAmused),
+                    (ex) => ex.Message).Value() == notAmused.Message);
+        }
+    }
+}
