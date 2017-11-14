c3aa9d1f31ac6d88ea40c861ebe44eccd27acb40
CLE <christopher.leppel@icarus-consult.de>
2017-11-14 17:19:13 +0100

    feat: adds decorator for OutputTo to append (incl. test)
    closes #31

---
+++ src/Yaapii.Atoms/IO/AppendTo.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Yaapii.Atoms.Scalar;
+
+namespace Yaapii.Atoms.IO
+{
+    /// <summary>
+    /// Append <see cref="IOutput"/> to a target.
+    /// </summary>
+    public sealed class AppendTo : IOutput, IDisposable
+    {
+        private readonly IScalar<IOutput> _outputSc;
+
+        /// <summary>
+        /// a path
+        /// </summary>
+        /// <param name="path"></param>
+        public AppendTo(string path) : this(new Uri(path))
+        { }
+
+        /// <summary>
+        /// Append <see cref="IOutput"/> to a target file Uri.
+        /// </summary>
+        /// <param name="path">a file uri, retrieve with Path.GetFullPath(absOrRelativePath) or prefix with file://. Must be absolute</param>
+        public AppendTo(Uri path) : this(
+            () => new FileStream(Uri.UnescapeDataString(path.AbsolutePath), FileMode.OpenOrCreate))
+        { }
+
+        /// <summary>
+        /// Append <see cref="IOutput"/> to a target <see cref="StreamWriter"/>.
+        /// </summary>
+        /// <param name="wtr">a writer</param>
+        public AppendTo(StreamWriter wtr) : this(wtr, Encoding.UTF8)
+        { }
+
+        /// <summary>
+        /// Append <see cref="IOutput"/> to a target <see cref="StreamWriter"/>.
+        /// </summary>
+        /// <param name="wtr">a writer</param>
+        /// <param name="enc"><see cref="Encoding"/> of the writer</param>
+        public AppendTo(StreamWriter wtr, Encoding enc) : this(
+            new WriterAsOutputStream(wtr, enc))
+        { }
+
+        /// <summary>
+        /// Append <see cref="IOutput"/> to a target <see cref="Stream"/> returned by a <see cref="Func{TResult}"/>.
+        /// </summary>
+        /// <param name="fnc">target stream returning function</param>
+        public AppendTo(Func<Stream> fnc) : this(new OutputTo(fnc))
+        { }
+
+        /// <summary>
+        /// Append <see cref="IOutput"/> to a target <see cref="Stream"/>.
+        /// </summary>
+        /// <param name="stream">target stream</param>
+        public AppendTo(Stream stream) : this(new OutputTo(stream))
+        { }
+
+        /// <summary>
+        /// Append <see cref="IOutput"/> to a target <see cref="IOutput"/>.
+        /// </summary>
+        /// <param name="output">target output</param>
+        public AppendTo(IOutput output) : this(new ScalarOf<IOutput>(output))
+        { }
+
+        /// <summary>
+        /// Append <see cref="IOutput"/> to a target <see cref="IScalar{IOutput}"/>.
+        /// </summary>
+        /// <param name="outputSc">target output scalar</param>
+        private AppendTo(IScalar<IOutput> outputSc)
+        {
+            _outputSc = outputSc;
+        }
+
+        /// <summary>
+        /// Disposes the stream
+        /// </summary>
+        public void Dispose()
+        {
+            _outputSc.Value().Stream().Dispose();
+        }
+
+        /// <summary>
+        /// Get the stream to append
+        /// </summary>
+        /// <returns>the stream</returns>
+        public Stream Stream()
+        {
+            var result = _outputSc.Value().Stream();
+            result.Seek(0, SeekOrigin.End);
+
+            return result;
+        }
+    }
+}
--- tests/Yaapii.Atoms.Tests/IO/AppendToTests.cs
+++ tests/Yaapii.Atoms.Tests/IO/AppendToTests.cs
@@ -1,26 +1,4 @@
-﻿// MIT License
-//
-// Copyright(c) 2017 ICARUS Consulting GmbH
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -30,7 +8,7 @@ using Yaapii.Atoms.Text;
 
 namespace Yaapii.Atoms.IO.Tests
 {
-    public sealed class OutputToTest
+    public sealed class AppendToTests
     {
         [Fact]
         public void WritesSimplePathContent()
@@ -39,18 +17,25 @@ namespace Yaapii.Atoms.IO.Tests
             var file = Path.GetFullPath(Path.Combine(temp.FullName, "file.txt"));
             if (File.Exists(file)) File.Delete(file);
 
-            String content = "Hello, товарищ!";
+            var content = "Hello, товарищ!";
+            var content2 = "Bye,  товарищ!";
             new LengthOf(
                 new TeeInput(
                     content,
                     new OutputTo(file))
                 ).Value();
 
+            new LengthOf(
+                new TeeInput(
+                    content2,
+                    new AppendTo(file))
+                ).Value();
+
             Assert.True(
                 new TextOf(
                     new InputAsBytes(
                         new InputOf(new Uri(file))))
-                .AsString() == content,
+                .AsString() == (content+content2),
                 "Can't write path content");
         }
 
@@ -64,18 +49,25 @@ namespace Yaapii.Atoms.IO.Tests
                 File.Delete(file.AbsolutePath);
             }
 
-            String txt = "Hello, друг!";
+            var txt = "Hello, друг!";
+            var txt2 = "Bye, друг!";
             new LengthOf(
                 new TeeInput(
                     txt,
                     new OutputTo(file))
             ).Value();
 
+            new LengthOf(
+                new TeeInput(
+                    txt2,
+                    new AppendTo(file))
+                ).Value();
+
             Assert.True(
                 new TextOf(
                     new InputAsBytes(
                         new InputOf(file)))
-                .AsString() == txt,
+                .AsString() == txt+txt2,
                 "Can't write file content");
         }
     }
