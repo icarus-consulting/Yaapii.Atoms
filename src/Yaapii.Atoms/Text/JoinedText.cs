cfa0b9b60446312150a654403e46c73dc2d3d0ad
Miro Tunk <miro.tunk@icarus-consult.de>
2017-11-20 17:08:38 +0100

    refactor: integrate envelopes

--- src/Yaapii.Atoms/Text/JoinedText.cs
+++ src/Yaapii.Atoms/Text/JoinedText.cs
@@ -58,10 +58,10 @@ namespace Yaapii.Atoms.Text
         public JoinedText(String delimit, IEnumerable<String> strs) :
             this(
                 new TextOf(delimit),
-                new Mapped<string, IText>(
-                    strs,
-                    new FuncOf<string, IText>((text) => new TextOf(text))
-                )
+                    new Enumerable.Mapped<string, IText>(
+                        strs,
+                        new FuncOf<string, IText>((text) => new TextOf(text))
+                    )
             )
         { }
 
@@ -132,7 +132,7 @@ namespace Yaapii.Atoms.Text
         {
             return String.Join(
                 this._delimiter.AsString(), 
-                new Mapped<IText, string>(
+                new Enumerable.Mapped<IText, string>(
                     this._texts.Value(),
                     text => text.AsString()
                     ));
