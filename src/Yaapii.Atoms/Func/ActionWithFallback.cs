d4d5ce7c239d0550e98cbaba647f37225ed87514
Christian Sauer <postancs@gmx.de>
2017-11-28 15:19:07 +0100

    feat: add ActionWithFallback

---
+++ src/Yaapii.Atoms/Func/ActionWithFallback.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yaapii.Atoms.Func
+{
+    /// <summary>
+    /// A Action that executes a callback if it fails (= an <see cref="Exception"/> occurs).
+    /// </summary>
+    public sealed class ActionWithFallback : IAction
+    {
+        private readonly IAction _func;
+        private readonly IAction<Exception> _fallback;
+
+        /// <summary>
+        /// A Action that executes a callback if it fails (= an <see cref="Exception"/> occurs).
+        /// </summary>
+        /// <param name="fnc">Action to call</param>
+        /// <param name="fbk">Fallback action</param>
+        public ActionWithFallback(IAction fnc, System.Action<Exception> fbk) : this(
+            fnc,
+            new ActionOf<Exception>(fbk))
+        { }
+        
+        /// <summary>
+        /// A Action that executes a callback if it fails (= an <see cref="Exception"/> occurs).
+        /// </summary>
+        /// <param name="fnc">Action to call</param>
+        /// <param name="fbk">Fallback action</param>
+        public ActionWithFallback(System.Action fnc, IAction<Exception> fbk) : this(
+            new ActionOf(fnc),
+            fbk)
+        { }
+        
+        /// <summary>
+        /// A Action that executes a callback if it fails (= an <see cref="Exception"/> occurs).
+        /// </summary>
+        /// <param name="fnc">Action to call</param>
+        /// <param name="fbk">Fallback action</param>
+        public ActionWithFallback(System.Action fnc, System.Action<Exception> fbk) : this(
+            new ActionOf(fnc),
+            new ActionOf<Exception>(fbk))
+        { }
+        
+        /// <summary>
+        /// A Action that executes a callback if it fails (= an <see cref="Exception"/> occurs).
+        /// </summary>
+        /// <param name="fnc">Action to call</param>
+        /// <param name="fbk">Fallback action</param>
+        public ActionWithFallback(IAction fnc, IAction<Exception> fbk)
+        {
+            this._func = fnc;
+            this._fallback = fbk;
+        }
+
+        /// <summary>
+        /// Invoke action
+        /// </summary>
+        public void Invoke()
+        {
+            try
+            {
+                _func.Invoke();
+            }
+            catch (Exception ex)
+            {
+                _fallback.Invoke(ex);
+            }
+        }
+    }
+}
---
+++ tests/Yaapii.Atoms.Tests/Func/ActionWithFallbackTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Yaapii.Atoms.Func;
+
+namespace Yaapii.Atoms.Tests.Func
+{
+    public sealed class ActionWithFallbackTest
+    {
+        [Fact]
+        public void UsesParameterlessMainFunc()
+        {
+            int i = 0;
+            new ActionWithFallback(
+                    () => { i = 1; },
+                    ex => { i = 2; }
+                ).Invoke();
+
+            Assert.True(
+                i == 1,
+                "Cannot use main action");
+        }
+
+        [Fact]
+        public void UsesParameterlessFallbackFunc()
+        {
+            int i = 0;
+            new ActionWithFallback(
+                    () => { throw new Exception(); },
+                    ex => { i = 2; }
+                ).Invoke();
+
+            Assert.True(
+                i == 2,
+                "Cannot use fallback action");
+        }
+    }
+}
