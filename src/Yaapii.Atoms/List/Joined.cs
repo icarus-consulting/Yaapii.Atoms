ef1caa13f1710a0636c2131533e1f61ec79c03ab
Miro Tunk <miro.tunk@icarus-consult.de>
2017-11-30 17:36:22 +0100

    fix: joined did not join

--- src/Yaapii.Atoms/List/Joined.cs
+++ src/Yaapii.Atoms/List/Joined.cs
@@ -4,25 +4,36 @@ using System.Collections.Generic;
 using System.Text;
 using Yaapii.Atoms.Collection;
 using Yaapii.Atoms.Enumerable;
+using Yaapii.Atoms.Scalar;
 
 namespace Yaapii.Atoms.List
 {
+    /// <summary>
+    /// Multiple lists joined together as one.
+    /// </summary>
+    /// <typeparam name="T">type of items in list</typeparam>
     public sealed class Joined<T> : ListEnvelope<T>
     {
-
-        public Joined(params List<T>[] src) : this(new EnumerableOf<List<T>>(src))
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="src">The lists to join together</param>
+        public Joined(params IList<T>[] src) : this(new EnumerableOf<IList<T>>(src))
         { }
 
-        public Joined(IEnumerable<List<T>> src) : base(() =>
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="src">The lists to join together</param>
+        public Joined(IEnumerable<IList<T>> src) : base(() =>
             {
                 var blocking = new BlockingCollection<T>();
                 foreach (var lst in src)
                 {
-                    lst.AddRange(lst);
+                    new And<T>(item => blocking.Add(item), lst).Value();
                 }
 
                 return new ListOf<T>(blocking);
-                
             })
         { }
     }
