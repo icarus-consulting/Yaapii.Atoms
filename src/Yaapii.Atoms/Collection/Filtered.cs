a0e5f2b23ab5329a595381080672ae49f2a08b1b
CLE <christopher.leppel@icarus-consult.de>
2017-12-08 13:06:56 +0100

    refactor: changes ctors that at least 2 items must be given to it.

--- src/Yaapii.Atoms/Collection/Filtered.cs
+++ src/Yaapii.Atoms/Collection/Filtered.cs
@@ -6,26 +6,42 @@ using Yaapii.Atoms.Scalar;
 
 namespace Yaapii.Atoms.Collection
 {
+    /// <summary>
+    /// A filtered <see cref="ICollection{T}"/>.
+    /// Pass a filter function which will applied to all items, similar to List{T}.Where(...) in LinQ
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public sealed class Filtered<T> : CollectionEnvelope<T>
     {
-
-        public Filtered(Func<T, Boolean> func, params T[] src) : this(func, new EnumerableOf<T>(src))
+        /// <summary>
+        /// A filtered <see cref="ICollection{T}"/> which filters by the given condition <see cref="Func{In, Out}"/>.
+        /// </summary>
+        /// <param name="func">filter function</param>
+        /// <param name="item1">first item to filter</param>
+        /// <param name="item2">secound item to filter</param>
+        /// <param name="items">other items to filter</param>
+        public Filtered(Func<T, Boolean> func, T item1, T item2, params T[] items) :
+            this(func, new EnumerableOf<T>(new ScalarOf<IEnumerator<T>>(() =>
+            {
+                var lst = new List<T>() { item1, item2 };
+                lst.AddRange(items);
+                return lst.GetEnumerator();
+            })))
         { }
 
-        /**
-         * Ctor.
-         * @param src Source collection
-         * @param func Filter function
-         * @since 0.23
-         */
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="func">filter function</param>
+        /// <param name="src">Source collection</param>
         public Filtered(Func<T, Boolean> func, IEnumerator<T> src) : this(func, new EnumerableOf<T>(src))
         { }
 
-        /**
-         * Ctor.
-         * @param src Source collection
-         * @param func Filter function
-         */
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="func">filter function</param>
+        /// <param name="src">Source collection</param>
         public Filtered(Func<T, Boolean> func, IEnumerable<T> src) : base(new ScalarOf<ICollection<T>>(() => new CollectionOf<T>(
                  new Enumerable.Filtered<T>(
                      func, src
--- src/Yaapii.Atoms/Enumerable/Filtered.cs
+++ src/Yaapii.Atoms/Enumerable/Filtered.cs
@@ -53,9 +53,15 @@ namespace Yaapii.Atoms.Enumerable
         /// <summary>
         /// A filtered <see cref="IEnumerable{T}"/> which filters by the given condition <see cref="Func{In, Out}"/>.
         /// </summary>
-        /// <param name="items">items to filter</param>
         /// <param name="fnc">filter function</param>
-        public Filtered(Func<T, Boolean> fnc, params T[] items) : this(fnc, new EnumerableOf<T>(items))
+        /// <param name="item1">first item to filter</param>
+        /// <param name="item2">secound item to filter</param>
+        /// <param name="items">other items to filter</param>
+        public Filtered(Func<T, Boolean> fnc, T item1, T item2, params T[] items) : 
+            this(fnc, new EnumerableOf<T>(new ScalarOf<IEnumerator<T>>(() => {
+                var lst = new List<T>() { item1, item2 };
+                lst.AddRange(items);
+                return lst.GetEnumerator(); })))
         { }
 
         /// <summary>
