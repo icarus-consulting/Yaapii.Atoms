3bc20362ce8c5b4993ede410a1ef8b17e3736aa8
Miro Tunk <miro.tunk@icarus-consult.de>
2018-01-02 09:05:03 +0100

    feat: convert bytes to and from base64
    closes #128

---
+++ src/Yaapii.Atoms/Bytes/BytesBase64.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yaapii.Atoms.Bytes
+{
+    /// <summary>
+    /// Encodes all origin bytes using the Base64 encoding scheme.
+    /// </summary>
+    public sealed class BytesBase64 : IBytes
+    {
+        private readonly IBytes _bytes;
+
+        /// <summary>
+        /// Encoded origin bytes using the Base64 encoding scheme.
+        /// </summary>
+        /// <param name="bytes"></param>
+        public BytesBase64(IBytes bytes)
+        {
+            _bytes = bytes;
+        }
+
+        /// <summary>
+        /// The bytes encoded as Base64
+        /// </summary>
+        /// <returns></returns>
+        public byte[] AsBytes()
+        {
+            return 
+                Encoding.UTF8.GetBytes(
+                    Convert.ToBase64String(
+                        _bytes.AsBytes()
+                    )
+                );
+        }
+    }
+}
