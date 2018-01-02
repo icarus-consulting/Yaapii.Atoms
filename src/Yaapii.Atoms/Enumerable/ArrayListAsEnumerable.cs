a3e8911eac01241b5b5875c2a18c95fe664d2f3b
Miro Tunk <miro.tunk@icarus-consult.de>
2018-01-02 17:04:37 +0100

    feat: add ArrayList conversion classes
    closes #114

---
+++ src/Yaapii.Atoms/Enumerable/ArrayListAsEnumerable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using Yaapii.Atoms.Scalar;
+
+namespace Yaapii.Atoms.Enumerable
+{
+    /// <summary>
+    /// A <see cref="ArrayList"/> converted to IEnumerable&lt;object&gt;
+    /// </summary>
+    public sealed class ArrayListAsEnumerable : EnumerableEnvelope<object>
+    {
+        /// <summary>
+        /// A ArrayList converted to IEnumerable&lt;object&gt;
+        /// </summary>
+        /// <param name="src">source ArrayList</param>
+        public ArrayListAsEnumerable(ArrayList src) : base(new StickyScalar<IEnumerable<object>>(() =>
+        {
+            var blocking = new BlockingCollection<object>();
+            foreach (var lst in src)
+            {
+                new And<object>(item => blocking.Add(item), lst).Value();
+            }
+
+            return blocking;
+        }))
+        { }
+    }
+}
