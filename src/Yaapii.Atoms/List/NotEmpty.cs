9189eb3af6a4e03875d0411b48216b1fc80fe710
Christian Sauer <31921575+aveXcaesar@users.noreply.github.com>
2018-01-08 15:29:36 +0100

    NotEmpty decorator for enumerables, lists and collections (#121)
    * feat: NotEmpty decorator for enumerables, lists and collections with tests
    
    closes #118

---
+++ src/Yaapii.Atoms/List/NotEmpty.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Yaapii.Atoms.Error;
+using Yaapii.Atoms.Scalar;
+
+namespace Yaapii.Atoms.List
+{
+    /// <summary>
+    /// Ensures that <see cref="IList{T}" /> is not empty/>
+    /// </summary>
+    /// <typeparam name="T">Type of the list</typeparam>
+    public sealed class NotEmpty<T> : ListEnvelope<T>
+    {
+        /// <summary>
+        /// Ensures that <see cref="IList{T}" /> is not empty/>
+        /// </summary>
+        /// <param name="origin">List</param>
+        public NotEmpty(IList<T> origin) : this(
+            origin,
+            new Exception("List is empty"))
+        { }
+
+        /// <summary>
+        /// Ensures that <see cref="IList{T}" /> is not empty/>
+        /// </summary>
+        /// <param name="origin">List</param>
+        /// <param name="ex">Execption to be thrown if empty</param>
+        public NotEmpty(IList<T> origin, Exception ex) : base(new ScalarOf<IList<T>>(
+            () =>
+            {
+                new FailPrecise(
+                    new FailEmpty<T>(
+                        origin),
+                    ex).Go();
+
+                return origin;
+            }
+            ))
+        { }
+           }
+}
---
+++ tests/Yaapii.Atoms.Tests/List/NotEmptyTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Yaapii.Atoms.Enumerable;
+
+namespace Yaapii.Atoms.List.Tests
+{
+    public sealed class NotEmptyTest
+    {
+        [Fact]
+        public void EmptyCollectionThrowsExeption()
+        {
+            Assert.Throws<Exception>(() =>
+                new LengthOf(
+                    new NotEmpty<bool>(
+                        new ListOf<bool>()
+                    )).Value());
+        }
+
+        [Fact]
+        public void NotEmptyCollectionThrowsNoExeption()
+        {
+            Assert.True(
+                new LengthOf(
+                    new NotEmpty<bool>(
+                        new ListOf<bool>(false)
+                    )).Value() == 1);
+        }
+
+        [Fact]
+        public void EmptyCollectionThrowsCustomExeption()
+        {
+            Assert.Throws<OperationCanceledException>(() =>
+                new LengthOf(
+                    new NotEmpty<bool>(
+                        new ListOf<bool>(),
+                        new OperationCanceledException()
+                    )).Value());
+        }
+    }
+}
