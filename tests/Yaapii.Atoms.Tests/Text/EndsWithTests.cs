86815ff53d38974222006602c7fb538e5ed14071
Christian Sauer <postancs@gmx.de>
2018-03-08 11:08:10 +0100

    test: tests for EndsWith()

---
+++ tests/Yaapii.Atoms.Tests/Text/EndsWithTests.cs
@@ -0,0 +1,78 @@
+﻿using Xunit;
+using Yaapii.Atoms.Text;
+
+namespace Yaapii.Atoms.Tests.Text
+{
+    public sealed class EndsWithTests
+    {
+        [Fact]
+        public void FindsTextEnd()
+        {
+            var x =
+                new EndsWith(
+                    new TextOf("Im an text with a realy good end!"),
+                    new TextOf("od end!")
+                );
+            Assert.True(x.Value());
+        }
+
+        [Fact]
+        public void TakesTailAsString()
+        {
+            var x =
+                new EndsWith(
+                    new TextOf("Im an text with a realy good end!"),
+                    "od end!"
+                );
+            Assert.True(x.Value());
+        }
+
+        [Fact]
+        public void FailsOnWrongTail()
+        {
+            var x =
+                new EndsWith(
+                    new TextOf("Im an text with a realy good end!"),
+                    new TextOf("od end")
+                );
+            Assert.False(x.Value());
+        }
+
+        [Fact]
+        public void FailsOnCaseMismatch()
+        {
+            var x =
+                new EndsWith(
+                    new TextOf("Im an text with a realy good end!"),
+                    new TextOf("od enD!")
+                );
+            Assert.False(x.Value());
+        }
+
+        [Fact]
+        public void FindsMultilineTextEnd()
+        {
+            var x =
+                new EndsWith(
+                    new TextOf(
+                        "Im an text with a realy good end!" + System.Environment.NewLine +
+                        "But the end is here"),
+                    new TextOf("s here")
+                );
+            Assert.True(x.Value());
+        }
+
+        [Fact]
+        public void FailsOnWrongTailOfMultilineText()
+        {
+            var x =
+                new EndsWith(
+                    new TextOf(
+                        "Im an text with a realy good end!" + System.Environment.NewLine +
+                        "But the end is here"),
+                    new TextOf("od end!")
+                );
+            Assert.False(x.Value());
+        }
+    }
+}
