95f7841aaf9f8928ee237170450b429476fa9fb7
Christian Sauer <31921575+aveXcaesar@users.noreply.github.com>
2018-03-16 08:58:23 +0100

    New Text decorators StartsWith and EndsWith
    * style: remove regions
    
    closes #167

---
+++ src/Yaapii.Atoms/Text/EndsWith.cs
@@ -0,0 +1,67 @@
+﻿// MIT License
+//
+// Copyright(c) 2017 ICARUS Consulting GmbH
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System.Text.RegularExpressions;
+
+namespace Yaapii.Atoms.Text
+{
+    /// <summary>
+    /// Checks if a text ends with an given content.
+    /// </summary>
+    public sealed class EndsWith : IScalar<bool>
+    {
+        private readonly IText _text;
+        private readonly IText _tail;
+
+        /// <summary>
+        /// Checks if a <see cref="IText"/> ends with an given <see cref="string"/>
+        /// </summary>
+        /// <param name="text">Text to test</param>
+        /// <param name="tail">Ending content to use in the test</param>
+        public EndsWith(IText text, string tail) : this(
+            text,
+            new TextOf(tail)
+        )
+        { }
+
+        /// <summary>
+        /// Checks if a <see cref="IText"/> ends with an given <see cref="IText"/>
+        /// </summary>
+        /// <param name="text">Text to test</param>
+        /// <param name="tail">Ending content to use in the test</param>
+        public EndsWith(IText text, IText tail)
+        {
+            this._text = text;
+            this._tail = tail;
+        }
+
+        /// <summary>
+        /// Gets the result
+        /// </summary>
+        /// <returns>The result</returns>
+        public bool Value()
+        {
+            var regex = new Regex(Regex.Escape(this._tail.AsString()) + "$");
+            return regex.IsMatch(this._text.AsString());
+        }
+    }
+}
---
+++ tests/Yaapii.Atoms.Tests/Text/EndsWithTests.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+using Yaapii.Atoms.Text;
+
+namespace Yaapii.Atoms.Tests.Text
+{
+    public sealed class EndsWithTests
+    {
+        [Fact]
+        public void MatchesText()
+        {
+            var x =
+                new EndsWith(
+                    new TextOf("Im a text with a really good end!"),
+                    new TextOf("od end!")
+                );
+            Assert.True(x.Value());
+        }
+
+        [Fact]
+        public void MatchesString()
+        {
+            var x =
+                new EndsWith(
+                    new TextOf("Im a text with a really good end!"),
+                    "od end!"
+                );
+            Assert.True(x.Value());
+        }
+
+        [Fact]
+        public void DoesntMatch()
+        {
+            var x =
+                new EndsWith(
+                    new TextOf("Im a text with a really good end!"),
+                    new TextOf("od end")
+                );
+            Assert.False(x.Value());
+        }
+    }
+}
