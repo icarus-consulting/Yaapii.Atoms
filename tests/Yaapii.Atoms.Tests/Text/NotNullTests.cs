5805283cb5f4cfed21a178d05fbe5cfd2e6e8705
lwuLok <lukas.wulff@icarus-consult.de>
2018-05-23 12:13:46 +0200

    test Added NotNulTests

---
+++ tests/Yaapii.Atoms.Tests/Text/NotNullTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace Yaapii.Atoms.Text.Tests
+{
+    public class NotNullTests
+    {    
+        [Fact]
+        public void RejectsNull()
+        {
+            IText s = null;                        
+
+            Assert.Throws<IOException>(
+                ()=> new NotNullText(
+                    s
+                ).AsString()
+            );
+        }
+    }
+}
