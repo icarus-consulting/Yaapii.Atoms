4d531d0506fc730c66828e0ce70de32327e37a86
jaanpeeter <jan-peter.schulz@icarus-consult.de>
2018-05-23 12:13:30 +0200

    test: added NotNullTextTest

---
+++ tests/Yaapii.Atoms.Tests/Text/NotNullTextTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace Yaapii.Atoms.Text.Tests
+{
+    public class NotNullTextTest
+    {
+        [Fact]
+        public void RejectsNullText()
+        {
+            IText nullText = null;
+            var text = new NotNullText(nullText);
+            Assert.Throws<IOException>(
+                () => text.AsString()
+            );
+        }
+    }
+}
