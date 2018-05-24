742c572cbd5ee0134f226f20c2b78bd136205691
lwuLok <lukas.wulff@icarus-consult.de>
2018-05-24 17:26:28 +0200

    test: added tests for partitioned Enumerator and Enumerable

---
+++ tests/Yaapii.Atoms.Tests/Enumerable/PartitionedTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Yaapii.Atoms.Enumerable
+{
+    public class PartitionedTests
+    {
+        [Fact]
+        public void NewPartitionTest()
+        {                  
+            Assert.Equal(
+                2,
+                new LengthOf(
+                    new Partitioned<string>(
+                        1, 
+                        new List<string>() { "hokus", "pokus" }
+                    )
+                ).Value()
+            );
+        }
+    }
+}
---
+++ tests/Yaapii.Atoms.Tests/Enumerator/PartitionedTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Yaapii.Atoms.Enumerator
+{
+    public class PartitionedTests
+    {
+        [Fact]
+        public void CountsPartititions()
+        {
+            int size = 3;
+            IEnumerator<int> enumerator = new List<int>() {1,2,3,4,5,6,7,8,9,10}.GetEnumerator();
+
+
+            var partition = new Partitioned<int>(size, enumerator);
+
+            partition.MoveNext();
+            partition.MoveNext();
+            partition.MoveNext();
+            partition.MoveNext();
+
+            Assert.Equal(new List<int>() { 10}, partition.Current);
+        }
+
+
+        [Fact]
+        public void Partionate()
+        {
+            int size = 1;
+            IEnumerator<string> enumerator = new List<string>() { "Christian","Sauer"}.GetEnumerator();
+
+
+            var partition = new Partitioned<string>(size, enumerator);
+
+            partition.MoveNext();
+
+           // Assert.Equal(new List<string>() { "Christian", "Sauer" }, partition.Current);
+            Assert.Equal(new List<string>() { "Christian"}, partition.Current);
+        }
+
+        [Fact]
+        public void ThrowsExceptionOnInvalidSize()
+        {
+            int size = 0;
+            IEnumerator<string> enumerator = new List<string>() { }.GetEnumerator();
+                     
+            Assert.Throws<ArgumentException>(()=>
+            new Partitioned<string>(size, enumerator).MoveNext()
+            );
+        }
+
+        [Fact]
+        public void CurrentThrowsException()
+        {
+            int size = 2;
+            IEnumerator<string> enumerator = new List<string>() { }.GetEnumerator();
+
+            Assert.Throws<InvalidOperationException>(() =>
+            new Partitioned<string>(size, enumerator).Current
+            );
+        }
+
+        [Fact]
+        public void FailsOnEnd()
+        {
+            int size = 5;
+            IEnumerator<int> enumerator = new List<int>() { 1, 2, 3, 4, 5}.GetEnumerator();
+
+            var partition = new Partitioned<int>(size, enumerator);
+            
+            partition.MoveNext();
+            partition.MoveNext();            
+
+            Assert.Throws<InvalidOperationException>(()=>
+            partition.Current
+            );
+        }
+    }
+}
