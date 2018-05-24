9de857ea9b3b532149ec5b286157a1a09e87f31c
ADY <amos.dyba@icarus-consult.de>
2018-05-24 15:04:50 +0200

    feat: adds comparableTexts
    closes #204

---
+++ src/Yaapii.Atoms/Text/ComparableText.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Yaapii.Atoms.Error;
+
+namespace Yaapii.Atoms.Text
+{
+	public sealed class ComparableText : IText, IComparable
+
+	{
+		private readonly IText text;
+
+		public ComparableText(IText text)
+		{
+			this.text = text;
+		}
+
+		public string AsString()
+		{
+			return this.text.AsString();
+		}
+
+		public bool Equals(IText other)
+		{
+			return this.text.Equals(other);
+		}
+
+		public int CompareTo(object obj)
+		{
+			new FailNull(
+				obj as IText,
+				"Object can´t be casted as Itext"
+			).Go();
+			return this.text.AsString().CompareTo(((IText)obj).AsString());
+		}
+	}
+}
---
+++ tests/Yaapii.Atoms.Tests/Text/ComparableTextTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Yaapii.Atoms.Text.Tests
+{
+    public sealed class ComparableTextTests
+    {
+		[Fact]
+		public void Compares()
+		{
+			Assert.True(
+				new ComparableText(
+					new TextOf("Hallo Welt")
+				).CompareTo(
+					new TextOf("Tschüss Welt")
+				) <= 0 
+			);			
+		}
+
+		[Fact]
+		public void Equal()
+		{
+			Assert.True(
+				new ComparableText(
+					new TextOf("Timm")
+				).Equals(
+					new TextOf("Jan-Peter")
+				) == false
+			);
+		}
+
+		[Fact]
+		public void AsString()
+		{
+			Assert.True(
+				new ComparableText(
+					new TextOf("Timm")
+				).AsString()
+				== "Timm"
+			);
+		}
+    }
+}
