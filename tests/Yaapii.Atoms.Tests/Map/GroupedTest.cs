bee9890bb17c5e610da4036e2c35bd8852eb9d6a
MSE1188 <maximilian.segelken@icarus-consult.de>
2018-05-24 16:46:12 +0200

    test: added Test for Grouped Class

---
+++ tests/Yaapii.Atoms.Tests/Map/GroupedTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Yaapii.Atoms.Enumerable;
+using Yaapii.Atoms.Func;
+using Yaapii.Atoms.IO;
+using Yaapii.Atoms.List;
+using Yaapii.Atoms.Map;
+using Yaapii.Atoms.Number;
+
+namespace Yaapii.Atoms.Map.Tests
+{
+    public sealed class GroupedTest
+    {
+        [Fact]
+        public void GroupsListToDictionaryOfKeysAndLists()
+        {
+            var srcList = new ListOf<string>("ABC", "ABCD", "ABCDE");
+            var keyFunc =
+                new FuncOf<string, double>((str) =>
+                    new NumberOf(
+                        new IO.LengthOf(new InputOf(str)).Value()
+                    ).AsDouble()
+                );
+
+            var valueFunc =
+                new FuncOf<string, string>((str) =>
+                    "ica" + str
+                );
+            Assert.Equal(
+                "icaABCD",
+                new Grouped<string,double, string>(srcList, keyFunc, valueFunc)[3.0][1]
+            );
+        }
+    }
+}
