7f83c7102ed55b47e70930bc3306f00daa1113b8
MSE1188 <maximilian.segelken@icarus-consult.de>
2018-05-24 16:16:08 +0200

    style: added comments

--- src/Yaapii.Atoms/Map/Grouped.cs
+++ src/Yaapii.Atoms/Map/Grouped.cs
@@ -13,20 +13,32 @@ namespace Yaapii.Atoms.Map
     /// <typeparam name="Value"></typeparam>
     public sealed class Grouped<T, Key, Value> : MapEnvelope<Key, IList<Value>>
     {
-        public Grouped(IEnumerable<T> list, IFunc<T, Key> keyFunc, IFunc<T, Value> valueFunc) : this(
-            new ListOf<T>(list),
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="src">Source Enumerable</param>
+        /// <param name="keyFunc">Function to convert Source Type to Key Type</param>
+        /// <param name="valueFunc">Function to Convert Source Type to Key Týpe</param>
+        public Grouped(IEnumerable<T> src, IFunc<T, Key> keyFunc, IFunc<T, Value> valueFunc) : this(
+            new ListOf<T>(src),
             keyFunc,
             valueFunc
         )
         { }
 
-        public Grouped(IList<T> list, IFunc<T, Key> keyFunc, IFunc<T, Value> valueFunc) : base(
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="src">Source Enumerable</param>
+        /// <param name="keyFunc">Function to convert Source Type to Key Type</param>
+        /// <param name="valueFunc">Function to Convert Source Type to Key Týpe</param>
+        public Grouped(IList<T> src, IFunc<T, Key> keyFunc, IFunc<T, Value> valueFunc) : base(
             () =>
             {
                 Dictionary<Key, IList<Value>> temp = new Dictionary<Key, IList<Value>>();
-                foreach(var entry in list)
+                foreach(var entry in src)
                 {
-                    temp[keyFunc.Invoke(entry)] = new Mapped<T, Value>(valueFunc, list);
+                    temp[keyFunc.Invoke(entry)] = new Mapped<T, Value>(valueFunc, src);
                 }
                 return temp;
             }
