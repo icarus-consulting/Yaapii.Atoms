1b7cc8813218ff27c35261f74ec9f4850b28801a
MSE1188 <maximilian.segelken@icarus-consult.de>
2018-05-24 14:46:43 +0200

    feat: Added TextBase64 -Class, Renamed Base64DecodedText to Base64Text

---
+++ src/Yaapii.Atoms/Text/TextBase64.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Yaapii.Atoms.Bytes;
+using Yaapii.Atoms.IO;
+
+namespace Yaapii.Atoms.Text
+{
+    public sealed class TextBase64 : IText
+    {
+        private readonly IText origin;
+
+        /// <summary>
+        /// A <see cref="string"/> as string <see cref="IText"/>
+        /// </summary>
+        /// <param name="str">string to encode</param>
+        public TextBase64(String str) : this(new TextOf(str))
+        { }
+
+        /// <summary>
+        /// A <see cref="IText"/> as IText <see cref="IText"/>
+        /// </summary>
+        /// <param name="text">text to encode</param>
+        public TextBase64(IText text)
+        {
+            this.origin =
+                new TextOf(
+                    new BytesBase64(
+                        new BytesOf(text)
+                    )
+                );
+        }
+
+        public string AsString()
+        {
+            return origin.AsString();
+                
+        }
+
+        public bool Equals(IText other)
+        {
+            return origin.Equals(other);
+        }
+    }
+}
