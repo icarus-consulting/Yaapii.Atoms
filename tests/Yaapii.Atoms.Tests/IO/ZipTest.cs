4a83eaf919a364e6b644ca99daf7877ecff7dcc4
jaanpeeter <jan-peter.schulz@icarus-consult.de>
2018-05-28 13:41:47 +0200

    refactor: changes Zip Test Path from Temp to build directory

--- tests/Yaapii.Atoms.Tests/IO/ZipTest.cs
+++ tests/Yaapii.Atoms.Tests/IO/ZipTest.cs
@@ -12,7 +12,7 @@ namespace Yaapii.Atoms.IO.Tests
         [Fact]
         public void HasData()
         {
-            string folderPath = Path.GetTempPath() + "\\" + "ZipTestFolder";
+            string folderPath = Path.GetDirectoryName("ZipTests"); //Path.GetTempPath() + "\\" + "ZipTestFolder";
             try
             {
                 var folder = Directory.CreateDirectory(folderPath);
