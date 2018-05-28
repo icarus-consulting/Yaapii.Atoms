14fc8e1577bdca8101926ff50a72a7a601b08ae3
ADY <amos.dyba@icarus-consult.de>
2018-05-28 17:28:51 +0200

    feat: adds SyncText

---
+++ src/Yaapii.Atoms/Text/SyncText.cs
@@ -0,0 +1,60 @@
+﻿// MIT License
+//
+// Copyright(c) 2017 ICARUS Consulting GmbH
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yaapii.Atoms.Text
+{
+    public sealed class SyncText : IText
+    {
+        private readonly IText origin;
+        private readonly Object lck;
+
+        public SyncText(IText text) : this(text,text)
+        { }
+        
+        public SyncText(IText Text,Object lck)
+        {
+            this.origin = Text;
+            this.lck = lck;
+        }
+
+        public string AsString()
+        {
+            lock (this.lck)
+            {
+                return this.origin.AsString();
+            }
+        }
+
+        public bool Equals(IText other)
+        {
+            lock (this.lck)
+            {
+                return this.origin.Equals(other);
+            }
+        }
+    }
+}
