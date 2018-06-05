99f313026d733e600dc6a63912c1374b31976ea9
Christian Sauer <31921575+aveXcaesar@users.noreply.github.com>
2018-06-05 16:19:31 +0200

    fix: let faisl accept specific execeptions
    * feature: Fail classes accepts an specific exception now.
    
    closes #58
    
    * test: add tests for fail classes
    
    * fix: FailZero() throws execption if the number is zero.
    
    closes #180

---
+++ tests/Yaapii.Atoms.Tests/Error/FailNoNumberTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+
+namespace Yaapii.Atoms.Error.Tests
+{
+    public sealed class FailNoNumberTests
+    {
+        [Fact]
+        public void FailsWhenNoNumber()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new FailNoNumber("Not a number").Go()
+            );
+        }
+
+        [Fact]
+        public void ThrowsSpecificException()
+        {
+            Assert.Throws<IndexOutOfRangeException>(() =>
+                new FailNoNumber(
+                    "Not a number",
+                    new IndexOutOfRangeException()
+                ).Go()
+            );
+        }
+    }
+}
---
+++ tests/Yaapii.Atoms.Tests/Error/FailNullOrEmptyTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xunit;
+
+namespace Yaapii.Atoms.Error.Tests
+{
+    public sealed class FailNullOrEmptyTests
+    {
+        [Fact]
+        public void FailsWhenNull()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new FailNullOrEmpty(null).Go()
+            );
+        }
+
+        [Fact]
+        public void FailsWhenEmpty()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new FailNullOrEmpty(String.Empty).Go()
+            );
+        }
+
+        [Fact]
+        public void ThrowsSpecificException()
+        {
+            Assert.Throws<IndexOutOfRangeException>(() =>
+                new FailNullOrEmpty(
+                    String.Empty,
+                    new IndexOutOfRangeException()
+                ).Go()
+            );
+        }
+    }
+}
