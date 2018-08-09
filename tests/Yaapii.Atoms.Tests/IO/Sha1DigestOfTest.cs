90c92db81b66c9f6cbaee533091374d3505b565e
Cedric VERNOU <cedric.vernou@gmail.com>
2018-08-09 10:39:05 +0200

    feat: add Sha1DigestOf

---
+++ tests/Yaapii.Atoms.Tests/IO/Sha1DigestOfTest.cs
@@ -0,0 +1,75 @@
+// MIT License
+//
+// Copyright(c) 2017 ICARUS Consulting GmbH
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+using Yaapii.Atoms.IO;
+using Yaapii.Atoms.Text;
+
+namespace Yaapii.Atoms.IO.Tests
+{
+    public sealed class Sha1DigestOfTest
+    {
+        [Fact]
+        public void ChecksumOfEmptyString()
+        {
+           Assert.Equal(
+                "da39a3ee5e6b4b0d3255bfef95601890afd80709",
+                new HexOf(
+                    new BytesOf(new Sha1DigestOf(new InputOf(string.Empty)))
+                ).AsString()
+           );
+        }
+
+        [Fact]
+        public void ChecksumOfString()
+        {
+            Assert.Equal(
+                "2ef7bde608ce5404e97d5f042f95f89f1c232871",
+                new HexOf(
+                    new BytesOf(new Sha1DigestOf(new InputOf("Hello World!")))
+                ).AsString()
+            );
+        }
+
+        [Fact]
+        public void ChecksumFromFile()
+        {
+            Assert.Equal(
+                "34f80bdab9b93af514004f127e440139aad63e2d",
+                new HexOf(
+                    new BytesOf(
+                        new Sha1DigestOf(
+                            new ResourceOf(
+                                "IO/Resources/digest-calculation.txt",
+                                this.GetType()
+                            )
+                        )
+                    )
+                ).AsString()
+            );
+        }
+    }
+}
