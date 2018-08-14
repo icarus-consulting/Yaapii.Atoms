1a39dfcd5b3b7b5477284245a83a6c150f143a81
Christian Sauer <postancs@gmx.de>
2018-08-14 17:35:16 +0200

    refactor: replace Uri by string and add MIT header

--- src/Yaapii.Atoms/IO/TempDirectory.cs
+++ src/Yaapii.Atoms/IO/TempDirectory.cs
@@ -1,37 +1,57 @@
-﻿using System;
+﻿// MIT License
+//
+// Copyright(c) 2017 ICARUS Consulting GmbH
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
 using System.IO;
 
 namespace Yaapii.Atoms.IO
 {
+    /// <summary>
+    /// A directory that cleans up on dispose.
+    /// </summary>
     public sealed class TempDirectory : IScalar<DirectoryInfo>, IDisposable
     {
-        private readonly Uri path;
+        private readonly string path;
 
-        public TempDirectory(string path) : this(
-            new Uri(path)
-        )
-        { }
-
-        public TempDirectory(Uri path)
+        public TempDirectory(string path)
         {
             this.path = path;
         }
 
         public void Dispose()
         {
-            if (Directory.Exists(path.AbsolutePath))
+            if (Directory.Exists(path))
             {
-                DeleteDirectory(path.AbsolutePath);
+                DeleteDirectory(path);
             }
         }
 
         public DirectoryInfo Value()
         {
-            if (!Directory.Exists(this.path.AbsolutePath))
+            if (!Directory.Exists(this.path))
             {
-                Directory.CreateDirectory(this.path.AbsolutePath);
+                Directory.CreateDirectory(this.path);
             }
-            return new DirectoryInfo(this.path.AbsolutePath);
+            return new DirectoryInfo(this.path);
         }
 
         private void DeleteDirectory(string path)
@@ -40,18 +60,7 @@ namespace Yaapii.Atoms.IO
             {
                 DeleteDirectory(subDir);
             }
-            try
-            {
-                Directory.Delete(path, true);
-            }
-            catch (IOException)
-            {
-                Directory.Delete(path, true);
-            }
-            catch (UnauthorizedAccessException)
-            {
-                Directory.Delete(path, true);
-            }
+            Directory.Delete(path, true);
         }
     }
 }
