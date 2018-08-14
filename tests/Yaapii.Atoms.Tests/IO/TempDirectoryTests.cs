ea3175ef21f645662a54db88d637e7471700f0d4
Christian Sauer <postancs@gmx.de>
2018-08-14 16:31:05 +0200

    test: tests for TempDirectory
    closes #244

---
+++ tests/Yaapii.Atoms.Tests/IO/TempDirectoryTests.cs
@@ -0,0 +1,75 @@
+﻿using System.IO;
+using Xunit;
+
+namespace Yaapii.Atoms.IO.Tests
+{
+    public sealed class TempDirectoryTest
+    {
+        [Fact]
+        public void CreatesDirectory()
+        {
+            var path = Path.Combine(Path.GetTempPath(), "SunnyDirectory");
+            try
+            {
+                var td = new TempDirectory(path).Value();
+                Assert.True(
+                    Directory.Exists(path)
+                );
+            }
+            finally
+            {
+                if (Directory.Exists(path))
+                {
+                    Directory.Delete(path, true);
+                }
+            }
+        }
+
+        [Fact]
+        public void DeletesEmptyDirectory()
+        {
+            var path = Path.Combine(Path.GetTempPath(), "SunnyDirectory");
+            try
+            {
+                using (var td = new TempDirectory(path))
+                {
+                    td.Value();
+                }
+                Assert.False(
+                    Directory.Exists(path)
+                );
+            }
+            finally
+            {
+                if (Directory.Exists(path))
+                {
+                    Directory.Delete(path, true);
+                }
+            }
+        }
+
+        [Fact]
+        public void DeletesNotEmptyDirectory()
+        {
+            var path = Path.Combine(Path.GetTempPath(), "SunnyDirectory");
+            using (var td = new TempDirectory(path))
+            {
+                td.Value();
+                File.WriteAllBytes(Path.Combine(path, "rainig.txt"), new byte[] { 0xAB });
+            }
+            try
+            {
+                Assert.False(
+                    Directory.Exists(path)
+                );
+            }
+            finally
+            {
+                if (Directory.Exists(path))
+                {
+                    Directory.Delete(path, true);
+                }
+            }
+        }
+    }
+}
