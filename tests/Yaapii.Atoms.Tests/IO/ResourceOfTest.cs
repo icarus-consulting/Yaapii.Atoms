132ba50c0392ed4eee3eb60d201abfe5061f509b
Christian Sauer <christian.sauer@icarus-consult.de>
2018-11-29 16:40:33 +0100

    test: change resources folder structure

--- tests/Yaapii.Atoms.Tests/IO/ResourceOfTest.cs
+++ tests/Yaapii.Atoms.Tests/IO/ResourceOfTest.cs
@@ -29,68 +29,16 @@ namespace Yaapii.Atoms.IO.Tests
         }
 
         [Theory]
-        [InlineData("IO/Resources/1/A.2/test.txt")]
-        [InlineData("IO/Resources/1/A._2/test.txt")]
-        [InlineData("IO/Resources/1/test.txt")]
-        [InlineData("IO/Resources/_1/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/test.txt")]
-        [InlineData("IO/Resources/1/A.2/_A_/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/-/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/__/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/_id_/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/{/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/__/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/_a_/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/[/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/__/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/_B_/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/!/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/__/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/_C_/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/§/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/__/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/_D_/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/$/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/__/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/_E_/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/=/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/__/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/_F_/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/+/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/__/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/;G;/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/_G_/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/;G;/;/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/;G;/__/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/;G;/,H,/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/;G;/_H_/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/;G;/,H,/,/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/;G;/,H,/__/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/;G;/,H,/`I`/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/;G;/,H,/_I_/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/;G;/,H,/`I`/`/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/;G;/,H,/`I`/__/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/;G;/,H,/`I`/´J´/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/;G;/,H,/`I`/_J_/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/;G;/,H,/`I`/´J´/´/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/;G;/,H,/`I`/´J´/__/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/;G;/,H,/`I`/´J´/'K'/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/;G;/,H,/`I`/´J´/_K_/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/;G;/,H,/`I`/´J´/'K'/'/test.txt")]
-        [InlineData("IO/Resources/1/A.2/-A-/{id}/[a]/!B!/§C§/$D$/=E=/+F+/;G;/,H,/`I`/´J´/'K'/__/test.txt")]
-        public void FindsResourceWithSpecialCharacters(string name)
+        [InlineData("IO/Resources/1/A.2/-A-/-/{id}/{/[a]/[/!B!/!/§C§/§/$D$/$/=E=/=/+F+/+/;G;/;/,H,/,/`I`/`/´J´/´/'K'/'/test.txt")]
+        [InlineData("IO/Resources/_1/A._2/_A_/__/_id_/__/_a_/__/_B_/__/_C_/__/_D_/__/_E_/__/_F_/__/_G_/__/_H_/__/_I_/__/_J_/__/_K_/__/test.txt")]
+        public void FindsResourceWithSpecialCharactersNew(string name)
         {
             Assert.Equal(
                 "Hello from Embedded!",
                 new TextOf(
-                    new ResourceOf(name, this.GetType())
+                    new ResourceOf(
+                        name,
+                        this.GetType())
                 ).AsString()
             );
         }
