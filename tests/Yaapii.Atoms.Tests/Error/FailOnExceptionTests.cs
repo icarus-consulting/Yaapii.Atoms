898c6878408ce6c81f74bd6d1cb7bb1aa533ecba
icaOBU <olaf.buerger@icarus-consult.de>
2019-02-12 17:54:45 +0100

    test: adds tests

---
+++ tests/Yaapii.Atoms.Tests/Error/FailOnExceptionTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+
+namespace Yaapii.Atoms.Error.Tests
+{
+    public sealed class FailOnExceptionTests
+    {
+        [Fact]
+        public void ThrowsExpectedException()
+        {
+            Assert.Throws<InvalidOperationException>(
+              () => new FailOnException<ArgumentException>(
+                    () => throw new InvalidOperationException(),
+                    new ArgumentException()
+                ).Go()
+            );
+        }
+
+        [Fact]
+        public void ThrowsOriginalException()
+        {
+            Assert.Throws<ArgumentException>(
+              () => new FailOnException<InvalidOperationException>(
+                    () => throw new ArgumentException(),
+                    new InvalidOperationException()
+                ).Go()
+            );
+        }
+    }
+}
