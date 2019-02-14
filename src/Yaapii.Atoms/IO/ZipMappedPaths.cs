2e9c30f7bb6890cf70cf69004d8a8b242fd43552
jaanpeeter <jan-peter.schulz@icarus-consult.de>
2019-02-14 16:50:01 +0100

    feat: implement ZipMappedPaths test: implement ZipMappedPathsTests

---
+++ src/Yaapii.Atoms/IO/ZipMappedPaths.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using Yaapii.Atoms;
+using Yaapii.Atoms.IO;
+using Yaapii.Atoms.Scalar;
+
+namespace Yaapii.Atoms.IO
+{
+    /// <summary>
+    /// A zip input mapped from a given zip input
+    /// Maps the zip entry paths according to the given mapping function
+    /// </summary>
+    public sealed class ZipMappedPaths : IInput
+    {
+        private readonly IScalar<Stream> input;
+
+        /// <summary>
+        /// A zip input mapped from a given zip input
+        /// Maps the zip entry paths according to the given mapping function
+        /// </summary>
+        public ZipMappedPaths(Func<string, string> mapping, IInput zip)
+        {
+            this.input =
+                new StickyScalar<Stream>(() =>
+                {
+                    Stream inMemory = new ValidatedZip(zip).Stream();
+                    var newMemory = new MemoryStream();
+
+                    using (var archive = new ZipArchive(inMemory, ZipArchiveMode.Read, true))
+                    using (var newArchive = new ZipArchive(newMemory, ZipArchiveMode.Create, true))
+                    {
+                        foreach (var entry in archive.Entries)
+                        {
+                            Move(entry, newArchive, mapping);
+                        }
+                        inMemory.Position = 0;
+                    }
+                    newMemory.Seek(0, SeekOrigin.Begin);
+                    return newMemory;
+                });
+        }
+        public Stream Stream()
+        {
+            return this.input.Value();
+        }
+
+        private void Move(ZipArchiveEntry source, ZipArchive archive, Func<string, string> mapping)
+        {
+            var mapped = mapping(source.FullName);
+            using (var sourceStream = source.Open())
+            using (var stream = archive.CreateEntry(mapped).Open())
+            {
+                new Atoms.IO.LengthOf(
+                    new TeeInput(
+                        new InputOf(sourceStream),
+                        new OutputTo(stream)
+                    )
+                ).Value();
+            }
+        }
+    }
+}
