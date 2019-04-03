87fea888058065a8338302e972d0d486348186f2
Miro Tunk <miro.tunk@icarus-consult.de>
2019-04-03 20:23:58 +0200

    feat: new BowFunc

---
+++ src/Yaapii.Atoms/Func/BowAction.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yaapii.Atoms.Func
+{
+    /// <summary>
+    /// An action which waits for a trigger to return true before executing.
+    /// </summary>
+    public sealed class BowAction : IAction
+    {
+        private readonly Func<bool> trigger;
+        private readonly Action prepare;
+        private readonly Action shoot;
+        private readonly TimeSpan timeout;
+
+        /// <summary>
+        /// An action which waits for a trigger to return true before executing.
+        /// </summary>
+        public BowAction(Func<bool> trigger, Action shoot) : this(trigger, () => { }, shoot, new TimeSpan(0, 0, 5))
+        { }
+
+        /// <summary>
+        /// An action which waits for a trigger to return true before executing.
+        /// </summary>
+        public BowAction(Func<bool> trigger, Action shoot, TimeSpan timeout) : this(trigger, () => { }, shoot, timeout)
+        { }
+
+        /// <summary>
+        /// An action which waits for a trigger to return true before executing.
+        /// </summary>
+        public BowAction(Func<bool> trigger, Action prepare, Action shoot) : this(trigger, prepare, shoot, new TimeSpan(0, 0, 5))
+        { }
+
+        /// <summary>
+        /// An action which waits for a trigger to return true before executing.
+        /// </summary>
+        public BowAction(Func<bool> trigger, Action prepare, Action shoot, TimeSpan timeout)
+        {
+            this.trigger = trigger;
+            this.prepare = prepare;
+            this.shoot = shoot;
+            this.timeout = timeout;
+        }
+
+        public void Invoke()
+        {
+            this.prepare();
+            var timeout = DateTime.Now + this.timeout;
+            var completed = false;
+
+            var parallel =
+                new Task(() =>
+                    {
+                        while (true)
+                        {
+                            if (this.trigger.Invoke())
+                            {
+                                completed = true;
+                                this.shoot();
+                                break;
+                            }
+                            System.Threading.Thread.Sleep(1);
+                        }
+                    }
+                );
+            parallel.Start();
+            parallel.ContinueWith((t) =>
+                {
+                    throw t.Exception.InnerException;
+                },
+                TaskContinuationOptions.OnlyOnFaulted
+            );
+
+            while(DateTime.Now < timeout)
+            {
+                if (completed) break;
+            }
+
+            if (!completed)
+            {
+                throw new ApplicationException($"The task did not complete within {this.timeout.TotalMilliseconds}ms.");
+            }
+        }
+
+        private void Wait()
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// An action which waits for a trigger to return true before executing.
+    /// </summary>
+    public sealed class BowFunc<T> : IAction<T>
+    {
+        private readonly Func<bool> trigger;
+        private readonly Action prepare;
+        private readonly Action<T> shoot;
+        private readonly TimeSpan timeout;
+
+        /// <summary>
+        /// An action which waits for a trigger to return true before executing.
+        /// </summary>
+        public BowFunc(Func<bool> trigger, Action prepare, Action<T> shoot, TimeSpan timeout)
+        {
+            this.trigger = trigger;
+            this.prepare = prepare;
+            this.shoot = shoot;
+            this.timeout = timeout;
+        }
+
+        public void Invoke(T parameter)
+        {
+            this.prepare();
+            var timeout = DateTime.Now + this.timeout;
+            var completed = false;
+
+            var parallel =
+                new Task(() =>
+                {
+                    while (true)
+                    {
+                        if (this.trigger.Invoke())
+                        {
+                            completed = true;
+                            this.shoot(parameter);
+                            break;
+                        }
+                        System.Threading.Thread.Sleep(1);
+                    }
+                }
+                );
+            parallel.Start();
+            parallel.ContinueWith((t) =>
+            {
+                throw t.Exception.InnerException;
+            },
+                TaskContinuationOptions.OnlyOnFaulted
+            );
+
+            while (DateTime.Now < timeout)
+            {
+                if (completed) break;
+            }
+
+            if (!completed)
+            {
+                throw new ApplicationException($"The task did not complete within {this.timeout.TotalMilliseconds}ms.");
+            }
+        }
+    }
+}
---
+++ tests/Yaapii.Atoms.Tests/Func/BowActionTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Yaapii.Atoms.Func;
+using Yaapii.Atoms.Text;
+
+namespace Yaapii.Atoms.Tests.Func
+{
+    public sealed class BowActionTests
+    {
+        [Fact]
+        public void WaitsForTrigger()
+        {
+            var actions = new List<string>();
+            var count = 0;
+            new BowAction(
+                () => { actions.Add("ask trigger"); return count++ > 0; },
+                () => actions.Add("shoot")
+            ).Invoke();
+
+            Assert.Equal(
+                "ask trigger, ask trigger, shoot",
+                new JoinedText(", ", actions).AsString()
+            );
+        }
+
+        [Fact]
+        public void Prepares()
+        {
+            var actions = new List<string>();
+            var count = 0;
+            new BowAction(
+                () => { actions.Add("ask trigger"); return count++ > 0; },
+                () => actions.Add("prepare"),
+                () => actions.Add("shoot")
+            ).Invoke();
+
+            Assert.Equal(
+                "prepare, ask trigger, ask trigger, shoot",
+                new JoinedText(", ", actions).AsString()
+            );
+        }
+
+        [Fact]
+        public void CancelsAfterTimeout()
+        {
+            Assert.Throws<ApplicationException>(
+                () =>
+                    new BowAction(
+                        () => false,
+                        () => { },
+                        new TimeSpan(0, 0, 0, 0, 100)
+                    ).Invoke()
+            );
+        }
+    }
+}
