69fbc973d2ac15493013844a0c5809e1b4409a2f
Miro Tunk <miro.tunk@icarus-consult.de>
2019-04-29 21:32:49 +0200

    feat: add strings

---
+++ src/Yaapii.Atoms/Enumerable/Strings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Yaapii.Atoms.Scalar;
+
+namespace Yaapii.Atoms.Enumerable
+{
+    public sealed class Strings : EnumerableEnvelope<string>
+    {
+        /// <summary>
+        /// Strings made from the ToString method of given objects.
+        /// </summary>
+        /// <param name="strings"></param>
+        public Strings(params object[] strings) : base(
+            new ScalarOf<IEnumerable<string>>(
+                new Mapped<object, string>(
+                    s => s.ToString(), 
+                    new EnumerableOf<object>(strings)
+                )
+            )
+        )
+        { }
+
+        /// <summary>
+        /// Enumerable of strings.
+        /// </summary>
+        /// <param name="strings"></param>
+        public Strings(params string[] strings) : base(new ScalarOf<IEnumerable<string>>(new EnumerableOf<string>(strings)))
+        { }
+    }
+}
---
+++ tests/Yaapii.Atoms.Tests/Enumerable/StringsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Yaapii.Atoms.Enumerable
+{
+    public sealed class StringsTests
+    {
+        [Fact]
+        public void Enumerates()
+        {
+            Assert.Equal(
+                new List<string>() { "one", "two", "eight" },
+                new Strings("one", "two", "eight")
+            );
+        }
+
+        [Fact]
+        public void Converts()
+        {
+            var number = 2.3f;
+            var enumerator = new Strings(number).GetEnumerator();
+            enumerator.MoveNext();
+            Assert.Equal(
+                number.ToString(),
+                enumerator.Current                
+            );
+        }
+    }
+}
