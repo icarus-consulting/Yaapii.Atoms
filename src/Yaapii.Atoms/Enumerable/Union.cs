bf47d3294558fca03489924a80b7300514404a8a
Miro Tunk <miro.tunk@icarus-consult.de>
2019-04-29 21:14:42 +0200

    feat: add union

---
+++ src/Yaapii.Atoms/Enumerable/Union.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Yaapii.Atoms.Enumerable
+{
+    /// <summary>
+    /// Union objects in two enumerables.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Union<T> : EnumerableEnvelope<T>
+    {
+        /// <summary>
+        /// Union objects in two enumerables.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public Union(IEnumerable<T> a, IEnumerable<T> b, Func<T, bool> compare) : base(() =>
+        {
+            var result = new List<T>();
+            foreach (var item in b)
+            {
+                if (new Contains<T>(a, compare).Value())
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        })
+        { }
+
+        /// <summary>
+        /// Union objects in two enumerables.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public Union(IEnumerable<T> a, IEnumerable<T> b) : base(() =>
+        {
+            var result = new List<T>();
+            foreach (var item in b)
+            {
+                if (new Contains<T>(a, item).Value())
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        })
+        { }
+
+        /// <summary>
+        /// Union objects in two enumerables.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        private Union(Func<IEnumerable<T>> unite) : base(unite)
+        { }
+    }
+}
---
+++ tests/Yaapii.Atoms.Tests/Enumerable/UnionTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Yaapii.Atoms.Enumerable.Test
+{
+    public sealed class UnionTests
+    {
+        [Fact]
+        public void Empty()
+        {
+            Assert.Empty(
+                new Union<string>(
+                    new EnumerableOf<string>("a", "b"),
+                    new EnumerableOf<string>("c")
+                )
+            );
+        }
+
+        [Theory]
+        [InlineData(new string[] { "a", "b" }, new string[] { "a", "b" }, new string[] { "a", "b" })]
+        [InlineData(new string[] { "a", "b" }, new string[] { "a" }, new string[] { "a" })]
+        public void MatchesString(IEnumerable<string> a, IEnumerable<string> b, IEnumerable<string> expected)
+        {
+            Assert.Equal(
+                expected,
+                new Union<string>(
+                   a, b
+                )
+            );
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 2 }, new int[] { 1, 2 }, new int[] { 1, 2 })]
+        [InlineData(new int[] { 1, 2 }, new int[] { 1 }, new int[] { 1 })]
+        public void MatchesInt(IEnumerable<int> a, IEnumerable<int> b, IEnumerable<int> expected)
+        {
+            Assert.Equal(
+                expected,
+                new Union<int>(
+                   a, b
+                )
+            );
+        }
+    }
+}
