03ee55b163ac75e6d9a4f557982905f16c5db274
Miro Tunk <miro.tunk@icarus-consult.de>
2019-04-29 21:22:33 +0200

    feat: add divergency

---
+++ src/Yaapii.Atoms/Enumerable/Divergency.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Yaapii.Atoms.Enumerable
+{
+    /// <summary>
+    /// Union objects in two enumerables.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Divergency<T> : EnumerableEnvelope<T>
+    {
+        /// <summary>
+        /// Union objects in two enumerables.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public Divergency(IEnumerable<T> a, IEnumerable<T> b, Func<T, bool> compare) : base(() =>
+        {
+            var result = new List<T>();
+            foreach (var item in b)
+            {
+                if (new Contains<T>(a, item).Value() != new Contains<T>(b, item).Value())
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        })
+        { }
+
+        /// <summary>
+        /// Union objects in two enumerables.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public Divergency(IEnumerable<T> a, IEnumerable<T> b) : base(() =>
+        {
+            var result = new List<T>();
+            foreach (var item in new Joined<T>(a, b))
+            {
+                if (new Contains<T>(a, item).Value() != new Contains<T>(b, item).Value())
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        })
+        { }
+
+        /// <summary>
+        /// Union objects in two enumerables.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        private Divergency(Func<IEnumerable<T>> unite) : base(unite)
+        { }
+    }
+}
