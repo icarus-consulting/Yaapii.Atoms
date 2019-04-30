fc6febf6c1e935cbd81c203ef55e20b3ee4cfebe
Miro Tunk <miro.tunk@icarus-consult.de>
2019-04-30 17:48:51 +0200

    fix: remove doubled ctors

--- src/Yaapii.Atoms/Enumerable/Strings.cs
+++ src/Yaapii.Atoms/Enumerable/Strings.cs
@@ -31,20 +31,6 @@ namespace Yaapii.Atoms.Enumerable
     public sealed class Strings : EnumerableEnvelope<string>
     {
         /// <summary>
-        /// Strings made from the ToString method of given objects.
-        /// </summary>
-        /// <param name="strings"></param>
-        public Strings(params object[] strings) : base(
-            new ScalarOf<IEnumerable<string>>(
-                new Mapped<object, string>(
-                    s => s.ToString(), 
-                    new EnumerableOf<object>(strings)
-                )
-            )
-        )
-        { }
-
-        /// <summary>
         /// Enumerable of strings.
         /// </summary>
         /// <param name="strings"></param>
