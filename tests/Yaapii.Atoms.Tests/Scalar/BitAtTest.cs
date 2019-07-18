3d6c0ab9b4c7cdcf545f11b033fb9b1d44abfcca
Christian Sauer <postancs@gmx.de>
2019-07-18 16:09:15 +0200

    test(BitAt): add tests

---
+++ tests/Yaapii.Atoms.Tests/Scalar/BitAtTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Xunit;
+using Yaapii.Atoms.Bytes;
+
+#pragma warning disable MaxPublicMethodCount // a public methods count maximum
+namespace Yaapii.Atoms.Scalar.Tests
+{
+    public sealed class BitAtTests
+    {
+        [Fact]
+        public void DeliversFirstElement()
+        {
+            Assert.True(
+                new BitAt(new BytesOf(1)).Value()
+            );
+        }
+
+        [Fact]
+        public void DeliversElementAtPosition()
+        {
+            Assert.True(
+                new BitAt(
+                    new BytesOf(2),
+                    1
+                ).Value()
+            );
+        }
+
+        [Fact]
+        public void FailsWhenBytesEmpty()
+        {
+            Assert.Throws<ArgumentException>(() =>
+                new BitAt(new BytesOf("")).Value()
+            );
+        }
+
+        [Fact]
+        public void DeliversFallback()
+        {
+            Assert.True(
+                new BitAt(
+                    new BytesOf(""),
+                    1,
+                    true
+                ).Value()
+            );
+        }
+
+        [Fact]
+        public void FallbackShowsError()
+        {
+            Assert.Throws<ArgumentException>(() =>
+                new BitAt(
+                    new BytesOf(""),
+                    1,
+                    (ex, bytes) => throw ex
+                ).Value()
+            );
+        }
+
+        [Fact]
+        public void UsesInjectedExcption()
+        {
+            Assert.Throws<ApplicationException>(() =>
+                new BitAt(
+                    new BytesOf(""),
+                    1,
+                    new ApplicationException()
+                ).Value()
+            );
+        }
+    }
+}
