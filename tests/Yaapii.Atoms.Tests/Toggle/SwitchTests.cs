82b5f33916cd2f290e6307b3fe658abe623217ce
Christian Sauer <postancs@gmx.de>
2019-07-18 11:47:24 +0200

    test: tests for Switch object

---
+++ tests/Yaapii.Atoms.Tests/Toggle/SwitchTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace Yaapii.Atoms.Toggle.Tests
+{
+    public sealed class SwitchTests
+    {
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void HasInitialState(bool initialState)
+        {
+            Assert.Equal(
+                initialState,
+                new Switch(initialState).IsOn()
+            );
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void Toggles(bool initialState)
+        {
+            var sw = new Switch(initialState);
+
+            sw.Toggle();
+            Assert.Equal(
+                !initialState,
+                sw.IsOn()
+            );
+        }
+    }
+}
