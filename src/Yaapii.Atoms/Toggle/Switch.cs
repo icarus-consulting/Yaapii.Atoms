01ef85b4eb5d3106062682ff6f64379af0417f92
Christian Sauer <postancs@gmx.de>
2019-07-18 11:47:05 +0200

    feat: new Switch implementing IToggle

---
+++ src/Yaapii.Atoms/Toggle/Switch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Yaapii.Atoms.Toggle
+{
+    /// <summary>
+    /// A toggable switch.
+    /// </summary>
+    public sealed class Switch : IToggle
+    {
+        private readonly List<bool> state;
+
+        /// <summary>
+        /// A toggable switch.
+        /// </summary>
+        /// <param name="isOn">The initial on state</param>
+        public Switch(bool isOn) : this(
+            new List<bool>() { isOn }
+        )
+        { }
+
+        private Switch(List<bool> state)
+        {
+            this.state = state;
+        }
+
+        public bool IsOn()
+        {
+            return this.state[0];
+        }
+
+        public void Toggle()
+        {
+            this.state[0] = !this.state[0];
+        }
+    }
+}
