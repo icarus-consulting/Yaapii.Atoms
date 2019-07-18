1351d2f9a87da1fb17de4485fa235a954f84cd3d
Christian Sauer <postancs@gmx.de>
2019-07-18 11:46:48 +0200

    feat: new IToggle interface

---
+++ src/Yaapii.Atoms/IToggle.cs
@@ -0,0 +1,18 @@
+﻿namespace Yaapii.Atoms
+{
+    /// <summary>
+    /// A toggable switch.
+    /// </summary>
+    public interface IToggle
+    {
+        /// <summary>
+        /// The on state.
+        /// </summary>
+        bool IsOn();
+
+        /// <summary>
+        /// Toggles the switch state.
+        /// </summary>
+        void Toggle();
+    }
+}
