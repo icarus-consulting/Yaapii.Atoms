af35eebf57540a6e8fa330565e2fe66484cbffaa
Christian Sauer <postancs@gmx.de>
2019-08-28 10:02:54 +0200

    test(BitAt): add tests for secondary ctors

--- tests/Yaapii.Atoms.Tests/Scalar/BitAtTest.cs
+++ tests/Yaapii.Atoms.Tests/Scalar/BitAtTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Xunit;
 using Yaapii.Atoms.Bytes;
+using Yaapii.Atoms.Func;
 
 #pragma warning disable MaxPublicMethodCount // a public methods count maximum
 namespace Yaapii.Atoms.Scalar.Tests
@@ -35,13 +36,67 @@ namespace Yaapii.Atoms.Scalar.Tests
         }
 
         [Fact]
+        public void FailsWithCustomExceptionOfFirstPosition()
+        {
+            Assert.Throws<ApplicationException>(() =>
+                new BitAt(new BytesOf(""), new ApplicationException("Custom exception message")).Value()
+            );
+        }
+
+        [Fact]
         public void DeliversFallback()
         {
             Assert.True(
                 new BitAt(
                     new BytesOf(""),
-                    1,
+                    2,
                     true
+                ).Value()
+            );
+        }
+
+        [Fact]
+        public void DeliversFallbackOnFirstPosition()
+        {
+            Assert.True(
+                new BitAt(
+                    new BytesOf(""),
+                    true
+                ).Value()
+            );
+        }
+
+        [Fact]
+        public void ExecutesFallbackFunc()
+        {
+            Assert.True(
+                new BitAt(
+                    new BytesOf(""),
+                    2,
+                    bytes => true
+                ).Value()
+            );
+        }
+
+        [Fact]
+        public void ExecutesFallbackIFunc()
+        {
+            Assert.True(
+                new BitAt(
+                    new BytesOf(""),
+                    2,
+                    new FuncOf<IBytes, bool>(bytes => true)
+                ).Value()
+            );
+        }
+
+        [Fact]
+        public void ExecutesFallbackFuncOnFirstPosition()
+        {
+            Assert.True(
+                new BitAt(
+                    new BytesOf(""),
+                    bytes => true
                 ).Value()
             );
         }
