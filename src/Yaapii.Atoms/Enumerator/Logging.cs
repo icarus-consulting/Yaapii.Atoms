b05c1a1d0a0f27ca72a9fac989385eaa99eaa900
Miro Tunk <miro.tunk@icarus-consult.de>
2019-09-09 17:30:31 +0200

    feat: add logging enumerable

---
+++ src/Yaapii.Atoms/Enumerator/Logging.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+#pragma warning disable NoProperties // No Properties
+namespace Yaapii.Atoms.Enumerable
+{
+    /// <summary>
+    /// An enumerator that logs the object T when it is moved.
+    /// </summary>
+    public sealed class LoggingEnumerator<T> : IEnumerator<T>
+    {
+        private readonly IEnumerator<T> origin;
+        private readonly Action<T> log;
+
+        /// <summary>
+        /// An enumerator that logs the object T when it is moved.
+        /// </summary>
+        public LoggingEnumerator(IEnumerator<T> origin, Action<T> log)
+        {
+            this.origin = origin;
+            this.log = log;
+        }
+
+        public T Current => this.origin.Current;
+
+        object IEnumerator.Current => this.Current;
+
+        public void Dispose()
+        {
+            this.origin.Dispose();
+        }
+
+        public bool MoveNext()
+        {
+            var moved = this.origin.MoveNext();
+            if (moved)
+            {
+                this.log(Current);
+            }
+            return moved;
+        }
+
+        public void Reset()
+        {
+            this.origin.Reset();
+        }
+    }
+}
