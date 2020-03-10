0eea22ae2af0ea89dd26c69dc872b92cdeeb3ae6
Edgar Toll <edgar.toll@icarus-consult.de>
2020-03-10 18:35:21 +0100

    test(KvpOf): knows about being lazy

--- tests/Yaapii.Atoms.Tests/Map/KvpOfTests.cs
+++ tests/Yaapii.Atoms.Tests/Map/KvpOfTests.cs
@@ -26,6 +26,18 @@ namespace Yaapii.Atoms.Lookup.Tests
         }
 
         [Fact]
+        public void KnowsAboutBeingLazy()
+        {
+            Assert.True(new Kvp.Of("2", () => "4").IsLazy());
+        }
+
+        [Fact]
+        public void KnowsAboutBeingNotLazy()
+        {
+            Assert.False(new Kvp.Of("2", "4").IsLazy());
+        }
+
+        [Fact]
         public void BuildsValueTyped()
         {
             Assert.Equal(
@@ -44,6 +56,18 @@ namespace Yaapii.Atoms.Lookup.Tests
         }
 
         [Fact]
+        public void KnowsAboutBeingLazyValue()
+        {
+            Assert.True(new Kvp.Of<int>("2", () => 4).IsLazy());
+        }
+
+        [Fact]
+        public void KnowsAboutBeingNotLazyValue()
+        {
+            Assert.False(new Kvp.Of<int>("2", 4).IsLazy());
+        }
+
+        [Fact]
         public void BuildsKeyValueTyped()
         {
             Assert.Equal(
@@ -59,6 +83,18 @@ namespace Yaapii.Atoms.Lookup.Tests
                 8,
                 new Kvp.Of<int, int>(8, () => throw new ApplicationException()).Key()
             );
+        }
+
+        [Fact]
+        public void KnowsAboutBeingLazyKeyValue()
+        {
+            Assert.True(new Kvp.Of<int, int>(2, () => 4).IsLazy());
+        }
+
+        [Fact]
+        public void KnowsAboutBeingNotLazyKeyValue()
+        {
+            Assert.False(new Kvp.Of<int, int>(2, 4).IsLazy());
         }
     }
 }
