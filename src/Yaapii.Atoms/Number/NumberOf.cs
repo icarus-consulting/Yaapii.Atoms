e9687ac92699826010f84d36ba166c6e6145b92b
hwi <henrik.wilshusen@icarus-consult.de>
2020-04-08 08:46:10 +0200

    refac: NumberOf uses Numberenvelope

--- src/Yaapii.Atoms/Number/NumberOf.cs
+++ src/Yaapii.Atoms/Number/NumberOf.cs
@@ -30,46 +30,55 @@ namespace Yaapii.Atoms.Number
     /// <summary>
     /// A parsed number
     /// </summary>
-    public sealed class NumberOf : INumber
+    public sealed class NumberOf : NumberEnvelope
     {
-        private readonly IScalar<long> _lng;
-        private readonly IScalar<float> _flt;
-        private readonly IScalar<int> _itg;
-        private readonly IScalar<double> _dbl;
-
         /// <summary>
         /// A <see cref="IText"/> as a <see cref="INumber"/>
         /// </summary>
         /// <param name="text">text to parse</param>
         /// <param name="blockSeperator">seperator for blocks, for example 1.000</param>
         /// <param name="decimalSeperator">seperator for floating point numbers, for example 16,235 </param>
-        public NumberOf(string text, string decimalSeperator, string blockSeperator) : this(
-            new ScalarOf<long>(() => Convert.ToInt64(
-                text,
-                new NumberFormatInfo()
-                {
-                    NumberDecimalSeparator = decimalSeperator,
-                    NumberGroupSeparator = blockSeperator
-                })),
-            new ScalarOf<int>(() => Convert.ToInt32(
-                text,
-                new NumberFormatInfo()
-                {
-                    NumberDecimalSeparator = decimalSeperator,
-                    NumberGroupSeparator = blockSeperator
-                })),
-            new ScalarOf<float>(() => (float)Convert.ToDecimal(text, new NumberFormatInfo()
-            {
-                NumberDecimalSeparator = decimalSeperator,
-                NumberGroupSeparator = blockSeperator
-            })),
-            new ScalarOf<double>(() => Convert.ToDouble(
-                text,
-                new NumberFormatInfo()
-                {
-                    NumberDecimalSeparator = decimalSeperator,
-                    NumberGroupSeparator = blockSeperator
-                }))
+        public NumberOf(string text, string decimalSeperator, string blockSeperator) : base(
+            new ScalarOf<double>(() =>
+                Convert.ToDouble(
+                    text,
+                    new NumberFormatInfo()
+                    {
+                        NumberDecimalSeparator = decimalSeperator,
+                        NumberGroupSeparator = blockSeperator
+                    }
+                )
+            ),
+            new ScalarOf<int>(() =>
+                Convert.ToInt32(
+                    text,
+                    new NumberFormatInfo()
+                    {
+                        NumberDecimalSeparator = decimalSeperator,
+                        NumberGroupSeparator = blockSeperator
+                    }
+                )
+            ),
+            new ScalarOf<long>(() =>
+                Convert.ToInt64(
+                    text,
+                    new NumberFormatInfo()
+                    {
+                        NumberDecimalSeparator = decimalSeperator,
+                        NumberGroupSeparator = blockSeperator
+                    }
+                )
+            ),
+            new ScalarOf<float>(() =>
+                (float)Convert.ToDecimal(
+                    text,
+                    new NumberFormatInfo()
+                    {
+                        NumberDecimalSeparator = decimalSeperator,
+                        NumberGroupSeparator = blockSeperator
+                    }
+                )
+            )
         )
         { }
 
@@ -93,168 +102,85 @@ namespace Yaapii.Atoms.Number
         /// </summary>
         /// <param name="str">The string</param>
         /// <param name="provider">a number format provider</param>
-        public NumberOf(string str, IScalar<IFormatProvider> provider) : this(
-            new ScalarOf<long>(
-                () =>
+        public NumberOf(string str, IScalar<IFormatProvider> provider) : base(
+            new ScalarOf<double>(() =>
+            {
+                try
                 {
-                    try
-                    {
-                        return Convert.ToInt64(str, provider.Value());
-                    }
-                    catch (FormatException)
-                    {
-                        throw new ArgumentException(new Formatted("'{0}' is not a number.", str).AsString());
-                    }
-                }),
-            new ScalarOf<int>(
-                () =>
+                    return Convert.ToDouble(str, provider.Value());
+                }
+                catch (FormatException)
                 {
-                    try
-                    {
-                        return Convert.ToInt32(str, provider.Value());
-                    }
-                    catch (FormatException)
-                    {
-                        throw new ArgumentException(new Formatted("'{0}' is not a number.", str).AsString());
-                    }
-                }),
-            new ScalarOf<float>(
-                () =>
+                    throw new ArgumentException(new Formatted("'{0}' is not a number.", str).AsString());
+                }
+            }),
+            new ScalarOf<int>(() =>
+            {
+                try
                 {
-                    try
-                    {
-                        return Convert.ToSingle(str, provider.Value());
-                    }
-                    catch (FormatException)
-                    {
-                        throw new ArgumentException(new Formatted("'{0}' is not a number.", str).AsString());
-                    }
-                }),
-            new ScalarOf<double>(
-                () =>
+                    return Convert.ToInt32(str, provider.Value());
+                }
+                catch (FormatException)
                 {
-                    try
-                    {
-                        return Convert.ToDouble(str, provider.Value());
-                    }
-                    catch (FormatException)
-                    {
-                        throw new ArgumentException(new Formatted("'{0}' is not a number.", str).AsString());
-                    }
-                })
-            )
+                    throw new ArgumentException(new Formatted("'{0}' is not a number.", str).AsString());
+                }
+            }),
+            new ScalarOf<long>(() =>
+            {
+                try
+                {
+                    return Convert.ToInt64(str, provider.Value());
+                }
+                catch (FormatException)
+                {
+                    throw new ArgumentException(new Formatted("'{0}' is not a number.", str).AsString());
+                }
+            }),
+            new ScalarOf<float>(() =>
+            {
+                try
+                {
+                    return Convert.ToSingle(str, provider.Value());
+                }
+                catch (FormatException)
+                {
+                    throw new ArgumentException(new Formatted("'{0}' is not a number.", str).AsString());
+                }
+            })
+        )
         { }
 
         /// <summary>
         /// A <see cref="int"/> as a <see cref="INumber"/>
         /// </summary>
         /// <param name="integer">The integer</param>
-        public NumberOf(int integer) : this(
-            new ScalarOf<long>(() => Convert.ToInt64(integer)),
-            new ScalarOf<int>(integer),
-            new ScalarOf<float>(() => Convert.ToSingle(integer)),
-            new ScalarOf<double>(() => Convert.ToDouble(integer))
-        )
+        public NumberOf(int integer) : base(integer)
         { }
 
         /// <summary>
         /// A <see cref="double"/> as a <see cref="INumber"/>
         /// </summary>
         /// <param name="dbl">The double</param>
-        public NumberOf(double dbl) : this(
-            new ScalarOf<long>(() => Convert.ToInt64(dbl)),
-            new ScalarOf<int>(() => Convert.ToInt32(dbl)),
-            new ScalarOf<float>(() => Convert.ToSingle(dbl)),
-            new ScalarOf<double>(dbl)
-            )
+        public NumberOf(double dbl) : base(dbl)
         { }
 
         /// <summary>
         /// A <see cref="long"/> as a <see cref="INumber"/>
         /// </summary>
         /// <param name="lng">The long</param>
-        public NumberOf(long lng) : this(
-            new ScalarOf<long>(() => lng),
-            new ScalarOf<int>(() => Convert.ToInt32(lng)),
-            new ScalarOf<float>(() => Convert.ToSingle(lng)),
-            new ScalarOf<double>(() => Convert.ToDouble(lng))
-            )
+        public NumberOf(long lng) : base(lng)
         { }
 
         /// <summary>
         /// A <see cref="float"/> as a <see cref="INumber"/>
         /// </summary>
         /// <param name="flt">The float</param>
-        public NumberOf(float flt) : this(
-            new ScalarOf<long>(() => Convert.ToInt64(flt)),
-            new ScalarOf<int>(() => Convert.ToInt32(flt)),
-            new ScalarOf<float>(flt),
-            new ScalarOf<double>(() => Convert.ToDouble(flt))
-            )
+        public NumberOf(float flt) : base(flt)
         { }
 
-        /// <summary>
-        /// A <see cref="IText"/> as a <see cref="INumber"/>
-        /// </summary>
-        /// <param name="lng"></param>
-        /// <param name="itg"></param>
-        /// <param name="flt"></param>
-        /// <param name="dbl"></param>
-        public NumberOf(IScalar<long> lng, IScalar<int> itg, IScalar<float> flt, IScalar<double> dbl)
-        {
-            _lng = lng;
-            _itg = itg;
-            _flt = flt;
-            _dbl = dbl;
-        }
-
-        /// <summary>
-        /// Number as double representation
-        /// Precision: ±5.0e−324 to ±1.7e308	(15-16 digits)
-        /// </summary>
-        /// <returns></returns>
-        public double AsDouble()
-        {
-            return _dbl.Value();
-        }
-
-        /// <summary>
-        /// Number as float representation
-        /// Precision: 	±1.5e−45 to ±3.4e38	    (7 digits)
-        /// </summary>
-        /// <returns></returns>
-        public float AsFloat()
-        {
-            return _flt.Value();
-        }
-
-        /// <summary>
-        /// Number as integer representation
-        /// Range -2,147,483,648 to 2,147,483,647	(Signed 32-bit integer)
-        /// </summary>
-        /// <returns></returns>
-        public int AsInt()
-        {
-            return _itg.Value();
-        }
-
-        /// <summary>
-        /// Number as long representation
-        /// Range -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807	(Signed 64-bit integer)
-        /// </summary>
-        /// <returns></returns>
-        public long AsLong()
-        {
-            return _lng.Value();
-        }
-
-        private ArgumentException ArgError(IText txt)
-        {
-            return
-                new ArgumentException(
-                    new Formatted("'{0}' is not a number.", txt.AsString()
-                ).AsString()
-            );
-        }
+        public NumberOf(IScalar<long> lng, IScalar<int> itg, IScalar<float> flt, IScalar<double> dbl) : base(
+            dbl, itg, lng, flt
+        )
+        { }
     }
 }
