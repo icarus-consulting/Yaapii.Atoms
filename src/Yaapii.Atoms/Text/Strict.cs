c7837260b1b75eb3aa99e0c8a577c394ffe4a57a
hwi <henrik.wilshusen@icarus-consult.de>
2020-04-29 12:39:01 +0200

    feat: strict text

---
+++ src/Yaapii.Atoms/Text/Strict.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Yaapii.Atoms.Enumerable;
+
+namespace Yaapii.Atoms.Text
+{
+    public sealed class Strict : TextEnvelope
+    {
+        /// <summary>
+        /// A strict text which can only be one of the specified valid texts, ignoring case.
+        /// </summary>
+        public Strict(string candidate, params string[] valid) : this(
+            candidate,
+            new LiveMany<string>(valid)
+        )
+        { }
+
+        /// <summary>
+        /// A strict text which can only be one of the specified valid texts, ignoring case.
+        /// </summary>
+        public Strict(string candidate, IEnumerable<string> valid) : this(
+            candidate,
+            true,
+            valid
+        )
+        { }
+
+        /// <summary>
+        /// A strict text which can only be one of the specified valid texts.
+        /// </summary>
+        public Strict(string candidate, bool ignoreCase, params string[] valid) : this(
+            candidate, ignoreCase, new LiveMany<string>(valid)
+        )
+        { }
+
+        /// <summary>
+        /// A strict text which can only be one of the specified valid texts.
+        /// </summary>
+        public Strict(string candidate, bool ignoreCase, IEnumerable<string> valid) : this(
+            new LiveText(candidate), ignoreCase, new Mapped<string, IText>((str) => new LiveText(str), valid)
+        )
+        { }
+
+        /// <summary>
+        /// A strict text which can only be one of the specified valid texts, ignoring case.
+        /// </summary>
+        public Strict(IText candidate, params string[] valid) : this(
+            candidate, true, valid
+        )
+        { }
+
+        /// <summary>
+        /// A strict text which can only be one of the specified valid texts.
+        /// </summary>
+        public Strict(IText candidate, bool ignoreCase, params string[] valid) : this(
+            candidate,
+            ignoreCase,
+            new Mapped<string, IText>((str) => new LiveText(str), valid)
+        )
+        { }
+
+        /// <summary>
+        /// A strict text which can only be one of the specified valid texts.
+        /// </summary>
+        public Strict(IText candidate, params IText[] valid) : this(candidate, true, valid)
+        { }
+
+        /// <summary>
+        /// A strict text which can only be one of the specified valid texts.
+        /// </summary>
+        public Strict(IText candidate, bool ignoreCase, params IText[] valid) : this(
+            candidate,
+            ignoreCase,
+            new LiveMany<IText>(valid)
+        )
+        { }
+
+        /// <summary>
+        /// A strict text which can only be one of the specified valid texts.
+        /// </summary>
+        public Strict(IText candidate, IEnumerable<IText> valid) : this(
+            candidate,
+            true,
+            valid
+        )
+        { }
+
+        /// <summary>
+        /// A strict text which can only be one of the specified valid texts.
+        /// </summary>
+        public Strict(IText candidate, bool ignoreCase, IEnumerable<IText> allowed) : this(
+            candidate,
+            allowed,
+            () => ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal)
+        { }
+
+        private Strict(IText candidate, IEnumerable<IText> allowed, Func<StringComparison> ignoreCase) : base(() =>
+        {
+            var valid = false;
+            var str = candidate.AsString();
+            foreach (var txt in allowed)
+            {
+                if (txt.AsString().Equals(str, ignoreCase()))
+                {
+                    valid = true;
+                    break;
+                }
+            }
+            if (!valid)
+            {
+                throw new ArgumentException($"'{str}' is not valid here - expected: {new Joined(", ", allowed).AsString()}");
+            }
+            return str;
+        }, false)
+        { }
+    }
+}
---
+++ tests/Yaapii.Atoms.Tests/Text/StrictTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Yaapii.Atoms.Text.Tests
+{
+    public sealed class StrictTest
+    {
+        [Fact]
+        public void Throws()
+        {
+            Assert.Throws<ArgumentException>(() =>
+                new Strict("not valid", "valid", "also valid").AsString()
+            );
+        }
+
+        [Fact]
+        public void ReturnsText()
+        {
+            var expected = "valid";
+            Assert.Equal(
+                expected,
+                new Strict(expected, "not valid", "also not", "valid", "ending").AsString()
+            );
+        }
+
+        [Fact]
+        public void IgnoresCase()
+        {
+            var expected = "LargeValid";
+            Assert.Equal(
+                expected,
+                new Strict(expected, "not valid", "also not", "LargeValid", "ending").AsString()
+            );
+        }
+
+        [Fact]
+        public void DoesNotIgnoresCase()
+        {
+            Assert.Throws<ArgumentException>(
+                () =>
+                new Strict("valid", false, "not valid", "also not", "largeValid", "ending").AsString()
+            );
+        }
+    }
+}
