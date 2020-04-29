7369c36af9c6c62a37cddc4fc129f906d58a556f
hwi <henrik.wilshusen@icarus-consult.de>
2020-04-29 13:04:03 +0200

    docs: headerdocs Strict text

--- src/Yaapii.Atoms/Text/Strict.cs
+++ src/Yaapii.Atoms/Text/Strict.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using Yaapii.Atoms.Enumerable;
+using Yaapii.Atoms.Scalar;
 
 namespace Yaapii.Atoms.Text
 {
+    /// <summary>
+    /// A strict text which can only be one of the specified valid texts.
+    /// </summary>
     public sealed class Strict : TextEnvelope
     {
         /// <summary>
         /// A strict text which can only be one of the specified valid texts, ignoring case.
         /// </summary>
+        /// <param name="candidate">The canidate to check for valid texts</param>
+        /// <param name="valid">The valid texts</param>
         public Strict(string candidate, params string[] valid) : this(
             candidate,
             new LiveMany<string>(valid)
@@ -19,6 +24,8 @@ namespace Yaapii.Atoms.Text
         /// <summary>
         /// A strict text which can only be one of the specified valid texts, ignoring case.
         /// </summary>
+        /// <param name="candidate">The canidate to check for valid texts</param>
+        /// <param name="valid">The valid texts</param>
         public Strict(string candidate, IEnumerable<string> valid) : this(
             candidate,
             true,
@@ -29,6 +36,9 @@ namespace Yaapii.Atoms.Text
         /// <summary>
         /// A strict text which can only be one of the specified valid texts.
         /// </summary>
+        /// <param name="candidate">The canidate to check for valid texts</param>
+        /// <param name="ignoreCase">Ignore case in the canidate and valid texts</param>
+        /// <param name="valid">The valid texts</param>
         public Strict(string candidate, bool ignoreCase, params string[] valid) : this(
             candidate, ignoreCase, new LiveMany<string>(valid)
         )
@@ -37,6 +47,9 @@ namespace Yaapii.Atoms.Text
         /// <summary>
         /// A strict text which can only be one of the specified valid texts.
         /// </summary>
+        /// <param name="candidate">The canidate to check for valid texts</param>
+        /// <param name="ignoreCase">Ignore case in the canidate and valid texts</param>
+        /// <param name="valid">The valid texts</param>
         public Strict(string candidate, bool ignoreCase, IEnumerable<string> valid) : this(
             new LiveText(candidate), ignoreCase, new Mapped<string, IText>((str) => new LiveText(str), valid)
         )
@@ -45,6 +58,8 @@ namespace Yaapii.Atoms.Text
         /// <summary>
         /// A strict text which can only be one of the specified valid texts, ignoring case.
         /// </summary>
+        /// <param name="candidate">The canidate to check for valid texts</param>
+        /// <param name="valid">The valid texts</param>
         public Strict(IText candidate, params string[] valid) : this(
             candidate, true, valid
         )
@@ -53,6 +68,9 @@ namespace Yaapii.Atoms.Text
         /// <summary>
         /// A strict text which can only be one of the specified valid texts.
         /// </summary>
+        /// <param name="candidate">The canidate to check for valid texts</param>
+        /// <param name="ignoreCase">Ignore case in the canidate and valid texts</param>
+        /// <param name="valid">The valid texts</param>
         public Strict(IText candidate, bool ignoreCase, params string[] valid) : this(
             candidate,
             ignoreCase,
@@ -61,14 +79,19 @@ namespace Yaapii.Atoms.Text
         { }
 
         /// <summary>
-        /// A strict text which can only be one of the specified valid texts.
+        /// A strict text which can only be one of the specified valid texts, ignoring case.
         /// </summary>
+        /// <param name="candidate">The canidate to check for valid texts</param>
+        /// <param name="valid">The valid texts</param>
         public Strict(IText candidate, params IText[] valid) : this(candidate, true, valid)
         { }
 
         /// <summary>
         /// A strict text which can only be one of the specified valid texts.
         /// </summary>
+        /// <param name="candidate">The canidate to check for valid texts</param>
+        /// <param name="ignoreCase">Ignore case in the canidate and valid texts</param>
+        /// <param name="valid">The valid texts</param>
         public Strict(IText candidate, bool ignoreCase, params IText[] valid) : this(
             candidate,
             ignoreCase,
@@ -77,8 +100,10 @@ namespace Yaapii.Atoms.Text
         { }
 
         /// <summary>
-        /// A strict text which can only be one of the specified valid texts.
+        /// A strict text which can only be one of the specified valid texts, ignoring case.
         /// </summary>
+        /// <param name="candidate">The canidate to check for valid texts</param>
+        /// <param name="valid">The valid texts</param>
         public Strict(IText candidate, IEnumerable<IText> valid) : this(
             candidate,
             true,
@@ -89,27 +114,36 @@ namespace Yaapii.Atoms.Text
         /// <summary>
         /// A strict text which can only be one of the specified valid texts.
         /// </summary>
-        public Strict(IText candidate, bool ignoreCase, IEnumerable<IText> allowed) : this(
+        /// <param name="candidate">The canidate to check for valid texts</param>
+        /// <param name="ignoreCase">Ignore case in the canidate and valid texts</param>
+        /// <param name="valid">The valid texts</param>
+        public Strict(IText candidate, bool ignoreCase, IEnumerable<IText> valid) : this(
             candidate,
-            allowed,
-            () => ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal)
+            valid,
+            new ScalarOf<StringComparison>(() => ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal))
         { }
 
-        private Strict(IText candidate, IEnumerable<IText> allowed, Func<StringComparison> ignoreCase) : base(() =>
+        /// <summary>
+        /// A strict text which can only be one of the specified valid texts.
+        /// </summary>
+        /// <param name="candidate">The canidate to check for valid texts</param>
+        /// <param name="valid">The valid texts</param>
+        /// <param name="stringComparer">Ignore case in the canidate and valid texts</param>
+        private Strict(IText candidate, IEnumerable<IText> valid, IScalar<StringComparison> stringComparer) : base(() =>
         {
-            var valid = false;
+            var result = false;
             var str = candidate.AsString();
-            foreach (var txt in allowed)
+            foreach (var txt in valid)
             {
-                if (txt.AsString().Equals(str, ignoreCase()))
+                if (txt.AsString().Equals(str, stringComparer.Value()))
                 {
-                    valid = true;
+                    result = true;
                     break;
                 }
             }
-            if (!valid)
+            if (!result)
             {
-                throw new ArgumentException($"'{str}' is not valid here - expected: {new Joined(", ", allowed).AsString()}");
+                throw new ArgumentException($"'{str}' is not valid here - expected: {new Joined(", ", valid).AsString()}");
             }
             return str;
         }, false)
