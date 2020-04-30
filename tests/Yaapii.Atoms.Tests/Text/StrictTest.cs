a056088f784e4cfbe37b57f93fd2b37e96212ba7
hwi <henrik.wilshusen@icarus-consult.de>
2020-04-30 12:20:20 +0200

    feat: more tests for strict text

--- tests/Yaapii.Atoms.Tests/Text/StrictTest.cs
+++ tests/Yaapii.Atoms.Tests/Text/StrictTest.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Xunit;
+using Yaapii.Atoms.Enumerable;
 
 namespace Yaapii.Atoms.Text.Tests
 {
@@ -41,6 +40,86 @@ namespace Yaapii.Atoms.Text.Tests
             Assert.Throws<ArgumentException>(
                 () =>
                 new Strict("valid", false, "not valid", "also not", "largeValid", "ending").AsString()
+            );
+        }
+
+        [Fact]
+        public void WorksWithList()
+        {
+            var expected = "TextWith!§$/()?`";
+            Assert.Equal(
+                expected,
+                new Strict(expected,
+                    new ManyOf("NotValid", expected)
+                ).AsString()
+            );
+        }
+
+        [Fact]
+        public void IsStickyByDefault()
+        {
+            var expected = "expected";
+            var counter = 0;
+            var text =
+                new Strict(new LiveText(() => expected),
+                    new LiveMany<IText>(
+                        new LiveText(() => counter++.ToString()),
+                        new TextOf(expected)
+                    )
+                );
+            text.AsString();
+            text.AsString();
+            Assert.Equal(
+                1,
+                counter
+            );
+        }
+
+        [Fact]
+        public void NotIgnoresCaseList()
+        {
+            var expected = "expected";
+            Assert.Equal(
+                expected,
+                new Strict(new TextOf(expected), false,
+                    new ManyOf<IText>(
+                        new TextOf("Expected"),
+                        new TextOf("Not Valid"),
+                        new TextOf(expected)
+                    )
+                ).AsString()
+            );
+        }
+
+        [Fact]
+        public void IgnoresCaseList()
+        {
+            var expected = "expected";
+            Assert.Equal(
+                expected,
+                new Strict(
+                    expected, true,
+                    new ManyOf(
+                        "Not Valid",
+                        "As well not valid",
+                        "ExpEcteD"
+                    )
+                ).AsString()
+            );
+        }
+
+        [Fact]
+        public void IgnoresCaseByDefault()
+        {
+            var expected = "expected";
+            Assert.Equal(
+                expected,
+                new Strict(
+                    expected,
+                    "Not Valid",
+                    "As well not valid",
+                    "ExpEcteD"
+                ).AsString()
             );
         }
     }
