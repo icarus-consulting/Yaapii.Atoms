9147212c57f6d6d4a0561f94d0a7449cbc267e3a
luplayer <lu-player@hotmail.de>
2020-09-24 10:18:49 +0200

    feat: add HeadOf class

---
+++ src/Yaapii.Atoms/IO/HeadOf.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Yaapii.Atoms.IO
+{
+    public sealed class HeadOf : IInput
+    {
+        private readonly IInput origin;
+        private readonly int length;
+
+        public HeadOf(IInput origin, int length)
+        {
+            this.origin = origin;
+            this.length = length;
+        }
+
+        public Stream Stream()
+        {
+            return new HeadInputStream(this.origin.Stream(), this.length);
+        }
+    }
+}
