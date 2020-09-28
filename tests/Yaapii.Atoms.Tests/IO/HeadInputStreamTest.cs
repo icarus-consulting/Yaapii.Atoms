2bebb0a7b1c58bba9ce8d947a0986533c79a04d9
luplayer <lu-player@hotmail.de>
2020-09-28 09:46:05 +0200

    tests: add tests for HeadInputStream and HeadOf

---
+++ tests/Yaapii.Atoms.Tests/IO/HeadInputStreamTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+using Yaapii.Atoms.IO;
+using Yaapii.Atoms.Text;
+
+namespace Yaapii.Atoms.IO.Tests
+{
+    public sealed class HeadInputStreamTest
+    {
+        [Fact]
+        void TestSkippingLessThanTotal()
+        {
+            var stream = 
+                new HeadInputStream(
+                    new InputOf("testSkippingLessThanTotal").Stream(),
+                    5
+                );
+
+            var skipped = stream.Seek(3L, SeekOrigin.Begin);
+
+            Assert.Equal(
+                3L,
+                skipped
+            );
+
+            Assert.Contains(
+                "tS",
+                new TextOf(new InputOf(stream)).AsString()
+            );
+        }
+
+        [Fact]
+        void TestSkippingMoreThanTotal()
+        {
+            var stream = 
+                new HeadInputStream(
+                    new InputOf("testSkippingMoreThanTotal").Stream(),
+                    5
+                );
+            var skipped = stream.Seek(7L, SeekOrigin.Begin);
+
+            Assert.Equal(
+                5L,
+                skipped
+            );
+
+            var input = new TextOf(stream).AsString();
+            Assert.Equal(
+                "",
+                input
+            );
+        }
+    }
+}
---
+++ tests/Yaapii.Atoms.Tests/IO/HeadOfTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Yaapii.Atoms.IO;
+using Yaapii.Atoms.Text;
+
+namespace Yaapii.Atoms.IO.Tests
+{
+    public sealed class HeadOfTest
+    {
+        [Fact]
+        void ReadsHeadOfLongerInput()
+        {
+            Assert.Contains(
+                "reads",
+                new TextOf(
+                    new HeadOf(
+                        new InputOf("readsHeadOfLongInput"),
+                        5
+                    )
+                ).AsString()
+            );
+        }
+
+        [Fact]
+        void ReadsOnlyLength()
+        {
+            var res =
+                new TextOf(
+                    new HeadOf(
+                        new InputOf("readsHeadOfLongInput"),
+                        5
+                    )
+                ).AsString();
+
+            Assert.Equal(
+                5,
+                new LengthOf(
+                    new InputOf(
+                        res
+                    )
+                ).Value()
+            );
+        }
+
+        [Fact]
+        void ReadsEmptyHeadOfInput()
+        {
+            Assert.Contains(
+                "",
+                new TextOf(
+                    new HeadOf(
+                        new InputOf("readsEmptyHeadOfInput"),
+                        0
+                    )
+                ).AsString()
+            );
+        }
+
+        [Fact]
+        void ReadsHeadOfShorterInput()
+        {
+            var input = "readsHeadOfShorterInput";
+            Assert.Contains(
+                input,
+                new TextOf(
+                    new HeadOf(
+                        new InputOf(input),
+                        35
+                    )
+                ).AsString()
+            );
+        }
+    }
+}
