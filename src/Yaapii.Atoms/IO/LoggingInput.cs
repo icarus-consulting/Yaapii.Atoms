823aac9e4264aa20609129f5eee05f3a726df4b7
luplayer <lu-player@hotmail.de>
2020-09-29 10:50:20 +0200

    feat: add LoggingInput and LoggingInputStream class

---
+++ src/Yaapii.Atoms/IO/LoggingInput.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Yaapii.Atoms.IO
+{
+    public sealed class LoggingInput : IInput
+    {
+
+        private readonly IInput origin;
+        private readonly string source;
+        private readonly Logger logger;
+
+        public LoggingInput(IInput input, string source) : this(input,source,Logger.GetLogger(source))
+        {}
+
+        public LoggingInput(IInput input, string source, Logger logger)
+        {
+            this.origin = input;
+            this.source = source;
+            this.logger = logger;
+        }
+
+        public Stream Stream()
+        {
+            return 
+                new LoggingInputStream(
+                    this.origin.Stream(),
+                    this.source,
+                    this.logger
+                );
+        }
+    }
+
+    internal class Logger
+    {
+        internal static Logger GetLogger(string source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Yaapii.Atoms/IO/LoggingInputStream.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Yaapii.Atoms.IO
+{
+    public sealed class LoggingInputStream : Stream
+    {
+
+        private readonly Stream origin;
+        private readonly string source;
+        private readonly Logger logger;
+
+
+
+
+        public override bool CanRead => throw new NotImplementedException();
+
+        public override bool CanSeek => throw new NotImplementedException();
+
+        public override bool CanWrite => throw new NotImplementedException();
+
+        public override long Length => throw new NotImplementedException();
+
+        public override long Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void Flush()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
