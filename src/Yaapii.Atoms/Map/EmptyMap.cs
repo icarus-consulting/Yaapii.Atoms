01a3d596152803aa7c591a8fafe8ff77268d04a2
icaOBU <olaf.buerger@icarus-consult.de>
2021-03-04 10:13:38 +0100

    feat: add emptymap

---
+++ src/Yaapii.Atoms/Map/EmptyMap.cs
@@ -0,0 +1,16 @@
+﻿using Yaapii.Atoms.Enumerable;
+
+namespace Yaapii.Atoms.Map
+{
+    /// <summary>
+    /// A map without entries
+    /// </summary>
+    public sealed class EmptyMap : MapEnvelope
+    {
+        /// <summary>
+        /// A map without entries
+        /// </summary>
+        public EmptyMap() : base(() => new MapOf(new ManyOf()), false)
+        { }
+    }
+}
---
+++ tests/Yaapii.Atoms.Tests/Map/EmptyMapTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Yaapii.Atoms.Map.Tests
+{
+    public sealed class EmptyMapTests
+    {
+        [Fact]
+        public void CreatesEmptyMap()
+        {
+            Assert.Empty(
+                new EmptyMap()
+            );
+        }
+    }
+}
