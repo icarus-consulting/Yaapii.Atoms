2c13d08374c02b6614300a12f710ad8d7e944c68
HerickJ-22 <henrik.wilshusen@icarus-consult.de>
2022-03-31 19:26:17 +0200

    fix: remove the added breaking change

--- src/Yaapii.Atoms/Map/MapOf.cs
+++ src/Yaapii.Atoms/Map/MapOf.cs
@@ -143,6 +143,17 @@ namespace Yaapii.Atoms.Map
         /// <summary>
         /// A map from string to string.
         /// </summary>
+        public MapOf(string key, string value, params string[] additional) : this(
+            new Enumerable.Joined<string>(
+                new ManyOf(key, value),
+                additional
+            )
+        )
+        { }
+
+        /// <summary>
+        /// A map from string to string.
+        /// </summary>
         /// <param name="pairSequence">Pairs as a sequence, ordered like this: key-1, value-1, ... key-n, value-n</param>
         public MapOf(IEnumerable<string> pairSequence) : this(
             () =>
