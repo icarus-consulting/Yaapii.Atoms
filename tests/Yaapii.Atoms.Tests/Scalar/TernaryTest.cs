c772aad11d2b2571595739609f72680d029e537f
Christian Sauer <postancs@gmx.de>
2025-05-14 11:25:21 +0200

    test(scalar): add ternary tests

--- tests/Yaapii.Atoms.Tests/Scalar/TernaryTest.cs
+++ tests/Yaapii.Atoms.Tests/Scalar/TernaryTest.cs
@@ -71,5 +71,52 @@ namespace Yaapii.Atoms.Scalar.Tests
                     input => input = 2
                 ).Value() == 8);
         }
+
+        //
+
+        [Fact]
+        public void ConditionTrueOneGeneric()
+        {
+            Assert.True(
+                new Ternary<int>(
+                    new True(),
+                    6,
+                    16
+                ).Value() == 6);
+        }
+
+        [Fact]
+        public void ConditionFalseOneGeneric()
+        {
+            Assert.True(
+                new Ternary<bool, int>(
+                    new False(),
+                    6,
+                    16
+                ).Value() == 16);
+        }
+
+        [Fact]
+        public void ConditionBooleanOneGeneric()
+        {
+            Assert.True(
+            new Ternary<bool, int>(
+                true,
+                6,
+                16
+            ).Value() == 6);
+        }
+
+        [Fact]
+        public void ConditionFuncOneGeneric()
+        {
+            Assert.True(
+                new Ternary<int, int>(
+                    5,
+                    input => input > 3,
+                    input => input = 8,
+                    input => input = 2
+                ).Value() == 8);
+        }
     }
 }
