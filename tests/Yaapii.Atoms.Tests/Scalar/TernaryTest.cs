43b951611f21d836ae26cef0a56636f27083a3c4
Christian Sauer <postancs@gmx.de>
2025-05-26 11:14:51 +0200

    test(ternary): fix tests

--- tests/Yaapii.Atoms.Tests/Scalar/TernaryTest.cs
+++ tests/Yaapii.Atoms.Tests/Scalar/TernaryTest.cs
@@ -35,7 +35,8 @@ namespace Yaapii.Atoms.Scalar.Tests
                     new True(),
                     6,
                     16
-                ).Value() == 6);
+                ).Value() == 6
+            );
         }
 
         [Fact]
@@ -46,18 +47,20 @@ namespace Yaapii.Atoms.Scalar.Tests
                     new False(),
                     6,
                     16
-                ).Value() == 16);
+                ).Value() == 16
+            );
         }
 
         [Fact]
         public void ConditionBoolean()
         {
             Assert.True(
-            new Ternary<bool, int>(
-                true,
-                6,
-                16
-            ).Value() == 6);
+                new Ternary<bool, int>(
+                    true,
+                    6,
+                    16
+                ).Value() == 6
+            );
         }
 
         [Fact]
@@ -69,10 +72,9 @@ namespace Yaapii.Atoms.Scalar.Tests
                     input => input > 3,
                     input => input = 8,
                     input => input = 2
-                ).Value() == 8);
+                ).Value() == 8
+            );
         }
-
-        //
 
         [Fact]
         public void ConditionTrueOneGeneric()
@@ -82,41 +84,32 @@ namespace Yaapii.Atoms.Scalar.Tests
                     new True(),
                     6,
                     16
-                ).Value() == 6);
+                ).Value() == 6
+            );
         }
 
         [Fact]
         public void ConditionFalseOneGeneric()
         {
             Assert.True(
-                new Ternary<bool, int>(
+                new Ternary<int>(
                     new False(),
                     6,
                     16
-                ).Value() == 16);
+                ).Value() == 16
+            );
         }
 
         [Fact]
         public void ConditionBooleanOneGeneric()
         {
             Assert.True(
-            new Ternary<bool, int>(
-                true,
-                6,
-                16
-            ).Value() == 6);
-        }
-
-        [Fact]
-        public void ConditionFuncOneGeneric()
-        {
-            Assert.True(
-                new Ternary<int, int>(
-                    5,
-                    input => input > 3,
-                    input => input = 8,
-                    input => input = 2
-                ).Value() == 8);
+                new Ternary<int>(
+                    true,
+                    6,
+                    16
+                ).Value() == 6
+            );
         }
     }
 }
