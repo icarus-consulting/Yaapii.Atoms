9cce3938f88ffca068855ceff36a1b2085ba1d76
Christian Sauer <christian.sauer@icarus-consult.de>
2026-02-11 15:57:57 +0100

    test(scalar): add test

---
+++ tests/Yaapii.Atoms.Tests/Scalar/DoIfNotNullTests.cs
@@ -0,0 +1,36 @@
+using Xunit;
+using Yaapii.Atoms.Scalar;
+
+namespace Yaapii.Atoms.Tests.Scalar
+{
+    public sealed class DoIfNotNullTests
+    {
+        [Fact]
+        public void ExecutesFunction()
+        {
+            Assert.Equal(
+                "executed",
+                new DoIfNotNull<string, string>(
+                    "input",
+                    "fallback",
+                    input => "executed"
+                ).Value()
+            );
+        }
+
+        [Fact]
+        public void DeliversFallback()
+        {
+            string nullValue = null;
+
+            Assert.Equal(
+                "fallback",
+                new DoIfNotNull<string, string>(
+                    nullValue,
+                    "fallback",
+                    input => "executed"
+                ).Value()
+            );
+        }
+    }
+}
